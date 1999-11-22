1a2b47d4d35d2de94da2fc7e2e71f16eeadd93a6
wvanstra <wvanstra>
1999-11-22 05:13:50 +0000

    moved from include/ repository

---
+++ Kernel/Formats/cpsr/class.h
@@ -0,0 +1,31 @@
+/*
+ * Macro to declare what class of machine is being compiled for.
+ */
+
+#ifndef _CLASS_H
+#define _CLASS_H
+
+#if pspm | pspm2
+
+/*
+ * Penn State Pulsar Machine (PSPM) derivative, i.e., analog filterbank.
+ */
+#define PSPM 1
+
+#elif nbpp | lbpp | bacspin | ebpp
+
+/*
+ * Berkeley Pulsar Processor (BPP) derivative, i.e., digital filter bank.
+ */
+#define BPP 1
+
+#elif cbr | cbrl | cpsr | tmfe
+
+/*
+ * Caltech Baseband Recorder (CBR) derivative, i.e., baseband recorder.
+ */
+#define CBR 1
+
+#endif
+
+#endif /* _CLASS_H */
