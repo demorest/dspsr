b34e8fa17b0afd7b7101b341a6e56086e933e0fa
redwards <redwards>
2000-01-24 14:16:13 +0000

    added obs joining stuff, see also genutil++/obsjoin

---
+++ Kernel/Formats/cpsr/dsp/cpsrobsjoin.h
@@ -0,0 +1,17 @@
+
+// This is a bunch of CPSR-specific stuff to go with the ObsJoin
+// stuff, see genutil++/obsjoin.{C,h}
+
+#include "obsjoin.h"
+
+namespace ObsJoin
+{
+  class CPSRFile : public LoadableSegment
+  {
+  public:
+    CPSRFile() {}
+    virtual bool load(const string& fname_); // return success status
+  };
+  typedef LoadableSegments<CPSRFile> CPSRFiles;
+}
+
