f7464e92e1a7f4e1d2d4f00dc3098462c37ab8e6
wvanstra <wvanstra>
2001-06-02 09:49:13 +0000

    various parsing

--- Kernel/Formats/cpsr/dsp/rdisk.h
+++ Kernel/Formats/cpsr/dsp/rdisk.h
@@ -1,14 +1,16 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/dspsr/dspsr/Kernel/Formats/cpsr/dsp/rdisk.h,v $
-   $Revision: 1.1 $
-   $Date: 2001/03/05 03:28:05 $
+   $Revision: 1.2 $
+   $Date: 2001/06/02 09:49:13 $
    $Author: wvanstra $ */
 
 #ifndef __RDISK_H
 #define __RDISK_H
 
+#include <iostream>
 #include <string>
+#include <vector>
 
 class rdisk {
 
@@ -16,13 +18,19 @@ class rdisk {
   string machine;
   string path;
 
+  rdisk () {};
   rdisk (const string& parse) { load (parse); }
 
   void load (const string& parse);
 
   // returns the available space in bytes
-  double space ();
+  double space () const;
+
+  static void load (vector<rdisk>& disks, const char* filename);
 
 };
+
+inline ostream& operator << (ostream& ostr, const rdisk& rd)
+{ return ostr << rd.machine << ":" << rd.path; }
 
 #endif
