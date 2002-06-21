16b554c4ff59d4eac6780becde25e78922ef4c97
wvanstra <wvanstra>
2002-06-21 06:53:24 +0000

    moved from baseband/timeseries

---
+++ Kernel/Formats/s2/s2obsjoin.C
@@ -0,0 +1,73 @@
+
+#include "s2obsjoin.h"
+
+#include <stdio.h>
+#include "tci_file.h"
+
+//#define DEBUG 1
+
+bool ObsJoin::s2File::load(const string& fname_)
+{
+  fname = fname_;
+
+  // Methods that are needed to extract two things
+  // Start time - as MJD
+  // Duration in seconds
+  //      Some day this should be made easier.....
+  // cwest
+
+  int nbits;
+  int npols = 2;
+  double rate;
+  float nbyte;
+
+  tci_fd s2file;
+  tci_hdr header;
+
+#ifdef DEBUG
+  fprintf(stderr, "Filename to load: \"%s\"\n", fname.c_str());
+#endif
+  if (tci_file_open (fname.c_str(), &s2file, &header, 'r') != 0){
+    fprintf(stderr, "ObsJoin:s2File::load - could not construct from %s\n",
+	    fname.c_str());
+    return false;
+  }
+
+  utc_t utc;
+  str2utc (&utc, header.hdr_time);
+  startMJD = MJD(utc);
+
+  char* bitspersample = strrchr (header.hdr_s2mode, '-');
+  if(!bitspersample){
+    cerr << "ObsJoin:s2File::load - trouble finding bits/sample in "
+	 << header.hdr_s2mode << "\n\t\tSetting to 1 bit/sample" << endl;
+    nbits = 1;
+  }else{
+    nbits = atoi(bitspersample+1);
+  }
+
+
+  nbyte = float(nbits*npols) / 8.0;
+  rate = double (s2file.data_rate) * 2.0 / nbyte;
+  duration = ((s2file.fsz * 16.0) / (nbits*npols)) / rate;
+
+#ifdef DEBUG
+  fprintf(stderr, "--------- START DEBUG of s2obsjoin_lib.C ------\n");
+  fprintf(stderr, "NByte    : %f\n", nbyte);
+  fprintf(stderr, "NBits    : %d\n", nbits);
+  fprintf(stderr, "NPols    : %d\n", npols);
+  
+  fprintf(stderr, "Rate     : %lf\n", rate);
+  fprintf(stderr, "s2Rate   : %d\n", s2file.data_rate);
+  fprintf(stderr, "s2fsz    : %ld\n", s2file.fsz);
+  
+  fprintf(stderr, "s2obsjoin: StartMJD = %s\n", startMJD.printall());
+  fprintf(stderr, "s2obsjoin: Duration = %lf\n", duration);
+  fprintf(stderr, "---------  END  DEBUG of s2obsjoin_lib.C ------\n");
+#endif
+  
+  tci_file_close(s2file);
+  
+  return true;
+}
+
---
+++ Kernel/Formats/s2/s2obsjoin.h
@@ -0,0 +1,17 @@
+
+// This is a bunch of S2-specific stuff to go with the ObsJoin
+// stuff, see genutil++/obsjoin.{C,h}
+
+#include "obsjoin.h"
+
+namespace ObsJoin
+{
+  class s2File : public LoadableSegment
+  {
+  public:
+    s2File() {}
+    virtual bool load(const string& fname_); // return success status
+  };
+  typedef LoadableSegments<s2File> s2Files;
+}
+
