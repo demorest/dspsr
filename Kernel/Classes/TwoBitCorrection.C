2cfb3a20681c1fcffe362bebd538d6533194390a
pulsar <pulsar>
2002-07-02 08:05:21 +0000

    Base class for Two Bit Correction Timeseries Operation

---
+++ Kernel/Classes/TwoBitCorrection.C
@@ -0,0 +1,178 @@
+#include <assert.h>
+
+#include "TwoBitCorrection.h"
+
+// #define _DEBUG 1
+
+// From Jenet&Anderson "The Effects of Digitization on Nonstationary
+// Stochastic Signals with Applications to Pulsar Signal Baseband Recording"
+// Table 1
+const double dsp::TwoBitCorrection::optimal_2bit_threshold = 0.9674;
+
+//! Null constructor
+dsp::TwoBitCorrection::TwoBitCorrection (const char* _name, Behaviour _type)
+  : Operation (_name, _type)
+{
+  nchannel  = -1;
+  maxstates = -1;
+
+  histograms = NULL;
+  dls_lookup = NULL;
+
+  nsample = 0;
+  cutoff_sigma = 0.0;
+  n_min = 0;
+  n_max = 0;
+}
+
+void dsp::TwoBitCorrection::destroy ()
+{
+  if (dls_lookup != NULL) delete [] dls_lookup; dls_lookup = NULL;
+  if (histograms != NULL) delete [] histograms; histograms = NULL;
+}
+
+void dsp::TwoBitCorrection::set_nchannel (int chan)
+{
+  if (chan != nchannel)
+    destroy();
+  nchannel = chan;
+}
+
+void dsp::TwoBitCorrection::set_nsample (int ppwt)
+{
+  if (ppwt != nsample)
+    destroy();
+  nsample = ppwt;
+}
+
+void dsp::TwoBitCorrection::set_cutoff_sigma (float cosig)
+{
+  if (cutoff_sigma != cosig)
+    destroy();
+  cutoff_sigma = cosig;
+}
+
+//
+// this function allocates arrays of the appropriate size
+// the following data members must be initialized before calling this
+// function:
+// nsample, n_min, n_max, maxstates, nchannel
+//
+void dsp::TwoBitCorrection::allocate ()
+{
+  if (verbose) cerr << "TwoBitCorrection::allocate enter\n";
+
+  int n_range = n_max - n_min + 1;
+
+#ifdef _DEBUG
+  cerr << "TwoBitCorrection::allocate"
+       << "\n range     = " << n_range
+       << "\n maxstates = " << maxstates
+       << "\n nsample  = " << nsample
+       << "\n nchannel  = " << nchannel
+       << endl;
+#endif
+
+  if (n_range < 2) {
+    fprintf (stderr, 
+	     "TwoBitCorrection::allocate invalid cutoff_sigma: %f\n",
+	     cutoff_sigma);
+    fprintf (stderr, "TwoBitCorrection::allocate nmin:%d  nmax:%d\n", 
+	     n_min, n_max); 
+    throw (string("TwoBitCorrection::allocate invalid"));
+  }
+
+  destroy ();
+
+  if (verbose) cerr << "TwoBitCorrection::allocate allocate buffers\n";
+  dls_lookup = new float [n_range * maxstates];
+  assert (dls_lookup != 0);
+
+  histograms = new unsigned long [nsample * nchannel];
+  assert (histograms != 0);
+
+  zero_histogram ();
+
+  if (verbose) cerr << "TwoBitCorrection::allocate exits\n";
+}
+
+void dsp::TwoBitCorrection::set_twobit_limits (int ppwt, float co_sigma)
+{
+  nsample = ppwt;
+  cutoff_sigma = co_sigma;
+
+  set_twobit_limits ();
+}
+
+void dsp::TwoBitCorrection::set_twobit_limits ()
+{
+  /* average number of samples that lay within the thresholds, x2 and x4,
+     where -x2 = x4 = 0.9674 of the noise power, as in Table 1 of 
+     Jenet&Anderson.  Apply t=0.9674sigma to Equation 45, 
+     (or -xl=xh=0.9674 to Equation A2) to get: */
+  float fraction_ones = erf(optimal_2bit_threshold/sqrt(2.0));
+
+  float n_ave = float(nsample) * fraction_ones;
+  // the root mean square deviation
+  float n_sigma = sqrt(nsample);
+
+  n_min = int (n_ave - (cutoff_sigma * n_sigma));
+  n_max = int (n_ave + (cutoff_sigma * n_sigma));
+
+  //fprintf (stderr, "TwoBitCorrection::set_twobit_limits n_min:%d n_max:%d\n",
+  //n_min, n_max);
+
+  if (n_max >= nsample) {
+    if (verbose)
+      cerr << "TwoBitCorrection::set_twobit_limits resetting nmax:"
+	   << n_max << " to nsample-2:" << nsample-1 << endl;
+    n_max = nsample-1;
+  }
+
+  if (n_min < 1) {
+    if (verbose)
+      cerr << "TwoBitCorrection::set_twobit_limits resetting nmin:"
+	   << n_min << " to one:1" << endl;
+    n_min = 1;
+  }
+
+  if (verbose) cerr << "TwoBitCorrection::set_twobit_limits nmin:"
+		    << n_min << " and nmax:" << n_max << endl;
+
+}
+
+void dsp::TwoBitCorrection::zero_histogram ()
+{
+  if (!histograms)
+    return;
+
+  int nbins = nsample * nchannel;
+  for (int ibin=0; ibin<nbins; ibin++)
+    histograms [ibin] = 0;
+}
+
+double dsp::TwoBitCorrection::get_histogram_mean (int channel)
+{
+  if (channel < 0 || channel >= nchannel) {
+    fprintf (stderr, "tbc_hist:: invalid channelnel # %d\n", channel);
+    throw (string("TwoBitCorrection::get_mean"));
+  }
+  double ones = 0.0;
+  double pts  = 0.0;
+
+  unsigned long* hist = histograms + channel*nsample;
+  for (int ival=0; ival<nsample; ival++) {
+    double samples = double (hist[ival]);
+    ones += samples * double (ival);
+    pts  += samples * double (nsample);
+  }
+  return ones/pts;
+}
+
+//! Calculate the mean voltage and power from Bit_Stream data
+int64 dsp::TwoBitCorrection::stats (vector<double>& mean,
+				    vector<double>& power)
+{
+  cerr << "TwoBitCorrection::stats not implemented" << endl;
+  return -1;
+}
---
+++ Kernel/Classes/dsp/TwoBitCorrection.h
@@ -0,0 +1,131 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/dspsr/dspsr/Kernel/Classes/dsp/TwoBitCorrection.h,v $
+   $Revision: 1.1 $
+   $Date: 2002/07/02 08:05:21 $
+   $Author: pulsar $ */
+
+#ifndef __TwoBitCorrection_h
+#define __TwoBitCorrection_h
+
+#include <vector>
+
+#include "Operation.h"
+#include "environ.h"
+
+namespace dsp {
+  
+  //! Converts a Timeseries containing digitized bits to floating point
+  /*! Thus far, only 2-bit sampling has been used and only 2-bit
+    conversion routines have been written.  These routines are
+    implemented by the TwoBitCorrection sub-classes and mostly perform
+    dynamic level-setting in order to correct for the effects of
+    digitization */
+  class TwoBitCorrection: public Operation {
+
+  public:
+
+    //! Optimal fraction of total power for 2-bit sampling threshold
+    static const double optimal_2bit_threshold;
+
+    //! Maintain a diagnostic histogram of digitizer statistics
+    static bool keep_histogram;
+
+    //! Null constructor
+    TwoBitCorrection (const char* name = "TwoBitCorrection",
+		   Behaviour type = outofplace);
+
+    //! Virtual destructor
+    virtual ~TwoBitCorrection () { destroy(); }
+
+    //! Return a descriptive string
+    virtual const string descriptor () const;
+
+    //! Initialize from a descriptor string as output by above
+    virtual void initialize (const string& descriptor);
+
+    //! Calculate the sum and sum-squared from each channel of digitized data
+    virtual int64 stats (vector<double>& sum, vector<double>& sumsq);
+
+    //! Get the number of digitizer channels
+    int get_nchannel () const { return nchannel; }
+
+    //! Set the number of digitizer channels
+    void set_nchannel (int);
+
+    //! Get the number of samples used to estimate undigitized power
+    int get_nsample () const { return nsample; }
+
+    //! Set the number of samples used to estimate undigitized power
+    void set_nsample (int);
+
+    //! Get the cut off power for impulsive interference excision
+    float get_cutoff_sigma() const { return cutoff_sigma; }
+
+    //! Set the cut off power for impulsive interference excision
+    void set_cutoff_sigma (float);
+
+    //! Get the minumum number of ones in nsample points
+    int get_nmin() const { return n_min; }
+
+    //! Get the maxumum number of ones in nsample points
+    int get_nmax() const { return n_max; }
+
+    //! Set n_min and n_max for twobit data
+    void set_twobit_limits (int nsample, float cutoff_sigma);
+
+    //! Sets limits with currently set parameters
+    void set_twobit_limits ();
+
+    //! Reset histogram counts to zero
+    void zero_histogram ();
+
+    //! Get the centroid of the histogram for the given digitizer channel
+    double get_histogram_mean (int channel);
+
+    //! Return a pointer to a new instance of the appropriate sub-class
+    static TwoBitCorrection* create (const Timeseries& input,
+				     int nsample=0, float cutoff_sigma=3.0);
+
+  protected:
+
+    //! Perform the bit conversion operation on the input Timeseries
+    virtual void operation ();
+
+    //! Unpacking algorithm is defined by sub-classes
+    virtual void unpack () = 0;
+
+    //! Number of digitizer channels
+    int nchannel;
+
+    //! Maximum number of digitizer output states: 2^nbit
+    int maxstates;
+
+    //! Histograms of number of ones in nsample points
+    unsigned long* histograms;
+
+    //! Values used in Dynamic Level Setting
+    float* dls_lookup;
+
+    //! Number of samples used to estimate undigitized power
+    int nsample;
+
+    //! Cut off power for impulsive interference excision
+    float cutoff_sigma;
+
+    //! Minumum number of ones in nsample points
+    int n_min;
+
+    //! Maximum number of ones in nsample points
+    int n_max;
+
+    //! Destroy allocated resources
+    void destroy ();
+
+    //! Allocate resources
+    void allocate ();
+  };
+  
+}
+
+#endif
