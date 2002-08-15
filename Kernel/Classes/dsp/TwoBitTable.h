40185e77da81c1b85739848dc144d82f506e7e3c
wvanstra <wvanstra>
2002-08-15 09:06:55 +0000

    added Unset

--- Kernel/Classes/dsp/TwoBitTable.h
+++ Kernel/Classes/dsp/TwoBitTable.h
@@ -1,9 +1,9 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/dspsr/dspsr/Kernel/Classes/dsp/TwoBitTable.h,v $
-   $Revision: 1.3 $
-   $Date: 2002/08/10 06:08:41 $
-   $Author: pulsar $ */
+   $Revision: 1.4 $
+   $Date: 2002/08/15 09:06:55 $
+   $Author: wvanstra $ */
 
 
 #ifndef __TwoBitTable_h
@@ -19,7 +19,7 @@ namespace dsp {
 
   public:
 
-    enum Type { OffsetBinary, SignMagnitude, TwosComplement };
+    enum Type { Unset, OffsetBinary, SignMagnitude, TwosComplement };
 
     //! Constructor
     TwoBitTable (Type type);
