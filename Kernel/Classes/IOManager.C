89c871bfdfc59ae9c4abf6957950338229bdac39
ahotan <ahotan>
2002-09-25 06:32:10 +0000

    DataManager name conflict with utils/genutil++/DataManager

---
+++ Kernel/Classes/IOManager.C
@@ -0,0 +1,146 @@
+#include "DataManager.h"
+#include "Timeseries.h"
+
+#include "CPSRFile.h"
+#include "CPSRTwoBitCorrection.h"
+
+#include "genutil.h"
+
+void dsp::DataManager::init()
+{
+  block_size = overlap = 0;
+  nsample = 512;
+}
+
+//! Constructor
+dsp::DataManager::DataManager ()
+{
+  init();
+}
+
+dsp::DataManager::~DataManager()
+{
+}
+
+//! Set the container from which input data will be read
+void dsp::DataManager::set_raw (Timeseries* _raw)
+{
+  raw = _raw;
+
+  if (converter)
+    converter -> set_input (raw);
+}
+
+//! Set the Input operator (should not normally need to be used)
+void dsp::DataManager::set_input (Input* _input)
+{
+  input = _input;
+
+  if (input) {
+    input->set_block_size (block_size);
+    input->set_overlap (overlap);
+  }
+}
+
+//! Set the conversion Operation (should not normally need to be used)
+void dsp::DataManager::set_converter (Operation* _converter)
+{
+  converter = _converter;
+
+  TwoBitCorrection* tbc = dynamic_cast<TwoBitCorrection*> (_converter);
+
+  if (tbc) {
+    tbc -> set_cutoff_sigma (3.0);
+    tbc -> set_nsample (nsample);
+  }
+}
+
+    //! Set the number of time samples to load on each call to load_data
+void dsp::DataManager::set_block_size (uint64 _block_size) 
+{ 
+  block_size = _block_size;
+  if (input)
+    input->set_block_size (block_size);
+}
+    
+//! Set the number of time samples by which consecutive blocks overlap
+void dsp::DataManager::set_overlap (uint64 _overlap) 
+{ 
+  overlap = _overlap;
+  if (input)
+    input->set_overlap (overlap);
+}
+    
+
+//! Set the number of samples used to estimate undigitized power
+void dsp::DataManager::set_nsample (int _nsample)
+{ 
+  nsample = _nsample;
+
+  TwoBitCorrection* tbc = dynamic_cast<TwoBitCorrection*> (converter.get());
+  if (tbc)
+    tbc -> set_nsample (nsample);
+}
+
+//! The operation loads the next block of data and converts it to float_Stream
+void dsp::DataManager::load (Timeseries* data)
+{
+  if (!input || !converter)
+    return;
+
+  if (!raw)
+    set_raw (new Timeseries);
+
+  input->load (raw);
+
+  converter->set_output (data);
+  converter->operate();
+}
+
+void dsp::DataManager::load_data (Timeseries* data)
+{
+  throw string ("DataManager::load_data run-time error");
+}
+
+//! Prepare the appropriate Input and converter Operation
+/*!
+
+  \param id string containing the id of the data source.  The source
+  id may be a:
+  <UL>
+  <LI> filename (for File)
+  <LI> a comma separated list of filenames (for MultiFile).
+  <LI> a string of the form "IPC:xx", where "xx" is a shmkey
+  </UL>
+
+  \pre This function is not fully implemented.
+*/
+void dsp::DataManager::open (const char* id) 
+{
+  File* loader = new CPSRFile;
+  loader->open(id);
+
+  set_input (loader);
+
+  TwoBitCorrection* tbc = new CPSRTwoBitCorrection;
+
+  set_converter (tbc);
+}
+
+//! End of data
+bool dsp::DataManager::eod()
+{
+  if (!input)
+    return true;
+
+  return input->eod();
+}
+    
+//! Seek to the specified time sample
+void dsp::DataManager::seek (int64 offset, int whence = 0)
+{
+  if (!input)
+    return;
+
+  input->seek (offset, whence);
+}
---
+++ Kernel/Classes/dsp/IOManager.h
@@ -0,0 +1,113 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/dspsr/dspsr/Kernel/Classes/dsp/IOManager.h,v $
+   $Revision: 1.1 $
+   $Date: 2002/09/25 06:32:10 $
+   $Author: ahotan $ */
+
+
+#ifndef __DataManager_h
+#define __DataManager_h
+
+#include "Input.h"
+
+namespace dsp {
+
+  class Operation;
+
+  //! Convenience interface to different file formats
+  /*! This class defines a common interface to backend-specific file
+    formats by constructing the appropriate header-loading and
+    data-unpacking routines.  */
+  class DataManager : public Input {
+
+  public:
+    
+    //! Verbosity flag
+    static bool verbose;
+    
+    //! Constructor
+    DataManager ();
+    
+    //! Destructor
+    virtual ~DataManager ();
+    
+    //! Prepare the appropriate Input and conversion Operation
+    virtual void open (const char* id);
+
+    //! Prepare the appropriate Input and conversion Operation
+    void open (const string& id) { open (id.c_str()); }
+
+    //! Return pointer to the appropriate Input
+    Input* get_input () const { return input; }
+    
+    //! Set the Input operator (should not normally need to be used)
+    void set_input (Input* input);
+
+    //! Return pointer to the appropriate conversion Operation
+    Operation* get_converter () const { return converter; }
+
+    //! Set the conversion Operation (should not normally need to be used)
+    void set_converter (Operation* converter);
+
+    // not sure about the rest of these
+
+    //! End of data
+    virtual bool eod();
+    
+    //! Load and convert the next block of data
+    virtual void load (Timeseries* data);
+
+    //! Seek to the specified time sample
+    virtual void seek (int64 offset, int whence = 0);
+    
+    //! Get the number of time samples to load on each call to load_data
+    virtual uint64 get_block_size () const { return block_size; }
+    //! Set the number of time samples to load on each call to load_data
+    virtual void set_block_size (uint64 _size);
+    
+    //! Return the number of time samples by which consecutive blocks overlap
+    virtual uint64 get_overlap () const { return overlap; }
+    //! Set the number of time samples by which consecutive blocks overlap
+    virtual void set_overlap (uint64 _overlap);
+    
+    //! Get the number of samples used to estimate undigitized power
+    virtual int get_nsample () const { return nsample; }
+
+    //! Set the number of samples used to estimate undigitized power
+    virtual void set_nsample (int _nsample);
+
+    //! Set the container into which input (raw) data will be read
+    virtual void set_raw (Timeseries* raw);
+
+  protected:
+
+    //! Null-define abstract method of the Input base class
+    void load_data (Timeseries* data);
+
+    //! Number of time samples to load on each load_block
+    uint64 block_size;
+
+    //! Number of time samples by which data blocks overlap
+    uint64 overlap;
+
+    //! Number of samples used to estimate undigitized power
+    int nsample;
+
+    //! Appropriate Input subclass
+    Reference::To<Input> input;
+
+    //! Appropriate converter Operation
+    Reference::To<Operation> converter;
+
+    //! The container in which the input (raw, unpacked) bitstream is stored
+    Reference::To<Timeseries> raw;
+
+  private:
+    void init();
+    
+  };
+
+}
+
+#endif // !defined(__DataManager_h)
