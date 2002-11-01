cb8455838ca1ea3d72de3d747b3bbd610e791b37
wvanstra <wvanstra>
2002-11-01 19:05:50 +0000

    set the total number of data points in the file

--- Kernel/Formats/cpsr2/CPSR2File.C
+++ Kernel/Formats/cpsr2/CPSR2File.C
@@ -6,6 +6,8 @@
 
 #include "CPSR2File.h"
 #include "CPSR2_Observation.h"
+#include "Error.h"
+
 #include "cpsr2_header.h"
 #include "yamasaki_verify.h"
 #include "genutil.h"
@@ -55,21 +57,36 @@ bool dsp::CPSR2File::is_valid (const char* filename) const
 void dsp::CPSR2File::open (const char* filename)
 {  
   if (get_header (cpsr2_header, filename) < 0)
-    throw_str ("CPSR2File::open - failed get_header(%s): %s",
-	       filename, strerror(errno));
+    throw Error (FailedCall, "dsp::CPSR2File::open",
+		 "get_header(%s) failed", filename);
   
   CPSR2_Observation data (cpsr2_header);
 
   if (yamasaki_verify (filename, data.offset_bytes, CPSR2_HEADER_SIZE) < 0)
-    throw_str ("cpsr2_Construct: YAMASAKI verification failed");
+    throw Error (FailedCall, "dsp::CPSR2File::open",
+		 "yamasaki_verify(%s) failed", filename);
 
   info = data;
 
   // re-open the file
   fd = ::open (filename, O_RDONLY);
   if (fd < 0)
-    throw_str ("CPSR2File::open - failed open(%s): %s", 
-	       filename, strerror(errno));
+    throw Error (FailedSys, "dsp::CPSR2File::open", 
+		 "open(%s) failed", filename);
+
+  struct stat buf;
+  if (fstat (fd, &buf) < 0)
+    throw Error (FailedSys, "dsp::CPSR2File::open", 
+		 "fstat(%s) failed", filename);
+
+  if (buf.st_size < CPSR2_HEADER_SIZE)
+    throw Error (InvalidState, "dsp::CPSR2File::open", 
+		 "file size=%d < CPSR2 header size=%d",
+		 buf.st_size, CPSR2_HEADER_SIZE);
+
+  uint64 total_bytes = buf.st_size - CPSR2_HEADER_SIZE;
+
+  info.set_ndat (info.nsamples (total_bytes));
 
   // set the number of bytes in header attribute
   header_bytes = CPSR2_HEADER_SIZE;
