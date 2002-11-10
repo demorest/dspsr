ebf5b5761d69cc2db7809c1986ea2316e12bbeee
wvanstra <wvanstra>
2002-11-10 21:10:21 +0000

    the simplest test

---
+++ Kernel/Applications/test_Unpack.C
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <unistd.h>
+
+#include "dsp/IOManager.h"
+#include "dsp/Unpacker.h"
+#include "dsp/TimeSeries.h"
+
+#include "string_utils.h"
+#include "dirutil.h"
+#include "Error.h"
+
+static char* args = "b:B:n:t:vV";
+
+void usage ()
+{
+  cout << "test_Fold - test phase coherent dedispersion kernel\n"
+    "Usage: test_Fold [" << args << "] file1 [file2 ...] \n"
+    " -b nbin\n"
+    " -B block_size  (in number of time samples)\n"
+    " -t blocks      (stop before the end of the file)\n"
+    " -n [1|2|4]     ndim kludge when forming stokes\n"
+       << endl;
+}
+
+int main (int argc, char** argv) 
+
+{ try {
+
+  char* metafile = 0;
+  bool verbose = false;
+
+  // number of time samples loaded from file at a time
+  int block_size = 512*1024;
+  int blocks = 0;
+
+  int c;
+  while ((c = getopt(argc, argv, args)) != -1)
+    switch (c) {
+
+    case 'V':
+      dsp::Observation::verbose = true;
+      dsp::Operation::verbose = true;
+    case 'v':
+      verbose = true;
+      break;
+
+    case 'B':
+      block_size = atoi (optarg);
+      break;
+
+    case 't':
+      blocks = atoi (optarg);
+      break;
+
+    default:
+      cerr << "invalid param '" << c << "'" << endl;
+    }
+
+  vector <string> filenames;
+
+  if (metafile)
+    stringfload (&filenames, metafile);
+  else 
+    for (int ai=optind; ai<argc; ai++)
+      dirglob (&filenames, argv[ai]);
+
+  if (filenames.size() == 0) {
+    usage ();
+    return 0;
+  }
+
+  if (verbose)
+    cerr << "Creating TimeSeries instance" << endl;
+  dsp::TimeSeries voltages;
+
+  if (verbose)
+    cerr << "Creating IOManager instance" << endl;
+
+  // Loader
+  dsp::IOManager manager;
+  manager.set_block_size (block_size);  
+
+
+  dsp::Operation::record_time = true;
+
+  for (unsigned ifile=0; ifile < filenames.size(); ifile++) try {
+
+    if (verbose)
+      cerr << "opening data file " << filenames[ifile] << endl;
+
+    manager.open (filenames[ifile]);
+
+    if (verbose)
+      cerr << "data file " << filenames[ifile] << " opened" << endl;
+
+    int block=0;
+
+    while (!manager.eod()) {
+
+      manager.load (&voltages);
+      
+      cerr << "check voltages " << voltages.get_state_as_string() 
+           << " block " << block << endl;
+      voltages.check();
+
+      block++;
+      cerr << "finished " << block << " blocks\r";
+      if (block == blocks) break;
+    }
+
+    if (verbose)
+      cerr << "end of data file " << filenames[ifile] << endl;
+
+    cerr << "Time spent converting data: " 
+	 << manager.get_unpacker()->get_total_time() << " seconds" << endl;
+
+  }
+  catch (string& error) {
+    cerr << error << endl;
+
+  }
+
+  
+  return 0;
+}
+
+catch (Error& error) {
+  cerr << "Error thrown: " << error << endl;
+  return -1;
+}
+
+catch (Reference::invalid& error) {
+  cerr << "Reference invalid exception thrown" << endl;
+  return -1;
+}
+
+catch (string& error) {
+  cerr << "exception thrown: " << error << endl;
+  return -1;
+}
+
+catch (...) {
+  cerr << "unknown exception thrown." << endl;
+  return -1;
+}
+
+}
