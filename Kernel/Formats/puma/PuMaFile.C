57ea082b56638f612f6f52860c3aed9e43ece606
wvanstra <wvanstra>
2003-04-24 17:31:28 +0000

    compiles and links

--- Kernel/Formats/puma/PuMaFile.C
+++ Kernel/Formats/puma/PuMaFile.C
@@ -1,7 +1,9 @@
-#include "PuMaFile.h"
-#include "PuMaObservation.h"
+#include "dsp/PuMaFile.h"
 
-#include "libpuma.h"
+#include "Error.h"
+#include "string_utils.h"
+
+#include <libpuma.h>
 
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -9,26 +11,49 @@
 #include <unistd.h>
 #include <errno.h>
 
+
+dsp::PuMaFile::PuMaFile (const char* filename)
+  : File ("PuMa")
+{
+  //! The PuMa header
+  header = malloc (sizeof(Header_type));
+
+  if (filename)
+    open (filename);
+}
+
+dsp::PuMaFile::~PuMaFile ()
+{
+  free (header);
+}
+
+bool dsp::PuMaFile::is_valid (const char* filename) const
+{ 
+  return false;
+}
+
 void dsp::PuMaFile::open_file (const char* filename)
 {
   if (verbose)
-    cerr << "dsp::PuMaFile::open " << filename << endl;
+    cerr << "dsp::PuMaFile::open_file " << filename << endl;
 
   FILE* fptr = fopen (filename, "r");
   if (!fptr)
     throw Error (FailedSys, "dsp::PuMaFile::open",
 		 "failed fopen(%s)", filename);
-  
+ 
+  Header_type* hdr = (Header_type*) hdr;
+
   // call the parsing routine from the puma library
-  prheader (&hdr, fptr);
+  prheader (hdr, fptr);
   
   if (verbose)
-    cerr << "dsp::PuMaFile::::openFile Skipping adjustments" << endl;
+    cerr << "dsp::PuMaFile::open_file Skipping adjustments" << endl;
   
   // skip over adjustments
   
   /* int ParBlkSize; Number of bytes in second (parameter) block */
-  unsigned nadjust = hdr.gen.ParBlkSize / (sizeof(Adjustments));
+  unsigned nadjust = hdr->gen.ParBlkSize / (sizeof(Adjustments));
   fseek(fptr, nadjust*sizeof(Adjustments), SEEK_CUR);
   
   header_bytes = ftell (fptr);
@@ -46,21 +71,147 @@ void dsp::PuMaFile::open_file (const char* filename)
     cerr << "dsp::PuMaFile::open exit" << endl;
 }
   
-void dsp::PuMaFile::set_info (const Header_type& hdr)
+void dsp::PuMaFile::set_info (const void* header)
 {
+  const Header_type* hdr = (const Header_type*) header;
+
   /* Boolean Raw;    In case of raw data */
-  if (hdr.redn.Raw != true)
+  if (hdr->redn.Raw != true)
     throw Error (InvalidState, "dsp::PuMaFile::set_info",
 		 "Data file does not contain raw data");
 
-  PuMa::set_observation (info, hdr);
+  /* Boolean Cluster[MAXFREQBANDS]; Is Data from this cluster in this file? */
+  unsigned iband = MAXFREQBANDS;
+  
+  for (unsigned i=0; i < MAXFREQBANDS; i++)
+    if (hdr->gen.Cluster[iband] == true) {
+      if (iband != MAXFREQBANDS)
+	throw Error (InvalidState, "parsePuMaHeader",
+		     "More than one cluster in data file");
+      iband = i;
+    }
+  
+  if (iband == MAXFREQBANDS)
+    throw Error (InvalidState, "parsePuMaHeader", "Cluster not set");
+  
+  /* Until further notice */
+  info.set_basis (Signal::Linear);
+
+  /* char ObsType[TXTLEN]; Research/test/calibration */
+  cerr << "parsePuMaHeader type = " << hdr->obs.ObsType << endl;
+  info.set_type (Signal::Pulsar);
+
+  unsigned npol_observed = 0;
+  bool invalid = false;
+  
+  /* int Nr; Mode of PuMa observation -1,0,1,2,4 */
+  if (hdr->mode.Nr == 0) {
+    
+    info.set_state (Signal::Nyquist);
+    
+    /* Boolean Xout;  Mode -1,0; X pol output    Mode 1,2,4  : FALSE */
+    if (hdr->mode.Xout)
+      npol_observed ++;
+    
+    /* Boolean Yout;  Mode -1,0; Y pol output    Mode 1,2,4  : FALSE */
+    if (hdr->mode.Yout)
+      npol_observed ++;
+    
+  }
+  else if (hdr->mode.Nr == 1) {
+    
+    /* Boolean Iout; Mode 1,2 ; I pol output    Mode -1,0,4 : FALSE */
+    /* Boolean Qout; Mode 1,2 ; Q pol output    Mode -1,0,4 : FALSE */
+    /* Boolean Uout; Mode 1,2 ; U pol output    Mode -1,0,4 : FALSE */
+    /* Boolean Vout; Mode 1,2 ; V pol output    Mode -1,0,4 : FALSE */
+    bool pol_vect = hdr->mode.Qout && hdr->mode.Uout && hdr->mode.Vout;
+    bool pol_some = hdr->mode.Qout || hdr->mode.Uout || hdr->mode.Vout;
+    
+    if (hdr->mode.Iout && pol_vect) {
+      info.set_state (Signal::Stokes);
+      npol_observed = 4;
+    }
+    else if (hdr->mode.Iout && !pol_some) {
+      info.set_state (Signal::Intensity);
+      npol_observed = 1;
+    }
+    else
+      invalid = true;
+    
+  }
+  else
+    invalid = true;
+  
+  if (invalid)
+    throw Error (InvalidState, "parsePuMaHeader", "unknown Mode");
+
+  if (info.get_state() == Signal::Nyquist)
+    info.set_mode ("Mode 0");
+  else
+    info.set_mode ("Mode 1");
+
+
+  info.set_npol (npol_observed);
+
+  /* int NFreqInFile; Mode 0-4 ; 1,2,4,8,16,..FreqChans in this file.
+     Normally one cluster per file is written. */
+  info.set_nchan (hdr->mode.NFreqInFile);
+  
+  /* for now data is always real-valued */
+  info.set_ndim (1);
+  
+  /* int BitsPerSamp; Number of output bits ; 1,2,4,8 */
+  info.set_nbit (hdr->mode.BitsPerSamp);
+
+  /* int DataBlkSize; Number of bytes in data block */
+  info.set_ndat( info.get_nsamples (hdr->gen.DataBlkSize) );
+  
+  string Westerbork = "WESTERBORK";
+    
+  /* char Name[NAMELEN];  Name of the observatory  */
+  if (hdr->obsy.Name != Westerbork)
+    throw Error (InvalidState, "parsePuMaHeader",
+		 "Observatory name != " + Westerbork);
+  
+  // Always Westerbork for now
+  info.set_telescope_code ('i');
+  
+  /* char Pulsar[NAMELEN]; Using the Taylor (1993) convention.
+     e.g. "PSR J0218+4232" */
+  string pulsar = hdr->src.Pulsar;
+  
+  // strip off the "PSR"
+  stringtok (&pulsar, " ");
+  cerr << "parsePuMaHeader source='" << pulsar << "'" << endl;
+  
+  info.set_source (pulsar);
+  
+  sky_coord position;
+  
+  /* double RA;  RA of the target (in radians)  */
+  position.ra().setRadians( hdr->src.RA );
+  /* double Dec; Dec of the target (in radians) */
+  position.dec().setRadians( hdr->src.Dec );
+  
+  info.set_coordinates( position );
+  
+  /* double SkyMidFreq; Mid sky frequency of band (in MHz) */
+  info.set_centre_frequency( hdr->WSRT.Band[iband].SkyMidFreq );
+  
+  double sign = 1.0;
+  /* Boolean NonFlip; Is band flipped (reverse freq order within band)? */
+  if (hdr->WSRT.Band[iband].NonFlip == false)
+    sign = -1.0;
+  
+  /* double Width; Width of the band (in MHz): 2.5, 5.0 or 10.0 */
+  info.set_bandwidth( sign * hdr->WSRT.Band[iband].Width );
 
   /* int DataMJD;     MJD of first sample of this data block */
   /* double DataTime; Time of first sample (fraction of day) */
-  info.set_start_time( MJD( hdr.gen.DataMJD, hdr.gen.DataTime ) );
+  info.set_start_time( MJD( hdr->gen.DataMJD, hdr->gen.DataTime ) );
   
   /* int Tsamp; Mode 0-4 ; output sample interval in nano sec */
-  double sampling_interval = 1e-9 * double(hdr.mode.Tsamp);
+  double sampling_interval = 1e-9 * double(hdr->mode.Tsamp);
   info.set_rate (1.0/sampling_interval);
   
   info.set_scale (1.0);
@@ -70,10 +221,10 @@ void dsp::PuMaFile::set_info (const Header_type& hdr)
   info.set_dc_centred (false);
   
   /* char ScanNum[NAMELEN]; FileSeriesNumber(FF)+Cluster(C). */
-  info.set_identifier (hdr.gen.ScanNum);
+  info.set_identifier (hdr->gen.ScanNum);
   
   /* char Platform[NAMELEN]; Name of machine: "PuMa" or "FFB" */
-  info.set_machine (hdr.gen.Platform);
+  info.set_machine (hdr->gen.Platform);
   
   info.set_dispersion_measure (0);
   info.set_between_channel_dm (0);
--- Kernel/Formats/puma/dsp/PuMaFile.h
+++ Kernel/Formats/puma/dsp/PuMaFile.h
@@ -1,16 +1,15 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/dspsr/dspsr/Kernel/Formats/puma/dsp/PuMaFile.h,v $
-   $Revision: 1.1 $
-   $Date: 2003/04/23 20:08:54 $
+   $Revision: 1.2 $
+   $Date: 2003/04/24 17:31:28 $
    $Author: wvanstra $ */
 
 
-#ifndef __PuMaFile_h
-#define __PuMaFile_h
+#ifndef __dsp_PuMaFile_h
+#define __dsp_PuMaFile_h
 
 #include "dsp/File.h"
-#include "pumadata.h"
 
 namespace dsp {
 
@@ -23,6 +22,9 @@ namespace dsp {
     //! Construct and open file
     PuMaFile (const char* filename = 0);
 
+    //! Destructor
+    ~PuMaFile ();
+
     //! Returns true if filename appears to name a valid PuMa file
     bool is_valid (const char* filename) const;
 
@@ -32,54 +34,12 @@ namespace dsp {
     void open_file (const char* filename);
 
     //! Set the Input::info attribute, given a loaded Header_type object
-    void set_info (const Header_type& hdr);
+    void set_info (const void* hdr);
 
   private:
 
     //! The PuMa header
-    Header_type hdr;
-
-#if FUTURE_WORK
-
-    /* In the future, a single PuMaFile may act like the current PuMaData
-       class developed by Russell Edwards.  ie. it will handle multiple
-       time divisions and multiple bands.  For now, I think it will be best
-       to use the MultiFile class in order to join contiguous time divisions.
-    */
-
-    //! Filename prefix
-    string fprefix;
-
-    //! Number of bands and starting band
-    int nbands, ibandstart;
-
-    //! Number of time divisions and starting division
-    int ntimediv, itimedivstart;
-
-    //! File descriptors for each band of the current time division
-    vector<FILE*> band_fptr;
-
-    //! Offset (in bytes) to first sample in each of the above files
-    vector<int> band_samplestart_offset;
-
-    //! Number of frequency channels per band
-    int nchan_band;
-
-    //! 
-    unsigned long current_offset;
-
-    //! 
-    int current_div;
-
-    //! Number of samples in each time div
-    vector<unsigned> offset_bydiv;
-
-    
-    //! Produce a string of the form ".00001.1.puma"
-    string make_fname_extension (int itimediv, int iband);
-
-
-#endif
+    void* header;
 
   };
 
