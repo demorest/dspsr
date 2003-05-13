b9f767d924d6717bead855e59aec64f4df2ab2ac
pulsar <pulsar>
2003-05-13 15:44:42 +0000

    COMPILE FIX: CPSRTwoBitCorrection methods

--- Kernel/Formats/cpsr/CPSRTwoBitCorrection.C
+++ Kernel/Formats/cpsr/CPSRTwoBitCorrection.C
@@ -16,32 +16,32 @@ dsp::CPSRTwoBitCorrection::CPSRTwoBitCorrection ()
 }
 
 /*! CPSR has four digitizers: 2 polns, I and Q */
-unsigned dsp::TwoBitCorrection::get_ndig () const
+unsigned dsp::CPSRTwoBitCorrection::get_ndig () const
 {
   return 4;
 }
 
 /*! Each 2-bit sample from each digitizer is packed into one byte */
-unsigned dsp::TwoBitCorrection::get_ndig_per_byte () const
+unsigned dsp::CPSRTwoBitCorrection::get_ndig_per_byte () const
 { 
   return 4;
 }
 
 
 /*! The quadrature components must be offset by one */
-unsigned dsp::TwoBitCorrection::get_output_offset (unsigned idig) const
+unsigned dsp::CPSRTwoBitCorrection::get_output_offset (unsigned idig) const
 {
   return idig % 2;
 }
 
 /*! The in-phase and quadrature components must be interleaved */
-unsigned dsp::TwoBitCorrection::get_output_incr () const
+unsigned dsp::CPSRTwoBitCorrection::get_output_incr () const
 {
   return 2;
 }
 
 /*! The first two digitizer channels are poln0, the last two are poln1 */
-unsigned dsp::TwoBitCorrection::get_output_ipol (unsigned idig) const
+unsigned dsp::CPSRTwoBitCorrection::get_output_ipol (unsigned idig) const
 {
   return idig / 2;
 }
@@ -55,7 +55,8 @@ unsigned dsp::TwoBitCorrection::get_output_ipol (unsigned idig) const
       2   1re = yI
       3   1im = yQ 
  */
-unsigned get_shift (unsigned idig, unsigned isamp)
+unsigned
+dsp::CPSRTwoBitCorrection::get_shift (unsigned idig, unsigned isamp) const
 {
   unsigned shift[4] = { 4, 6, 0, 2 };
 
