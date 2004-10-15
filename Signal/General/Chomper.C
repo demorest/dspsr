d06159ba20c4fe5cfec9fb563a5bcca1066e843c
hknight <hknight>
2004-10-15 01:20:19 +0000

    An almost trivial class

---
+++ Signal/General/Chomper.C
@@ -0,0 +1,24 @@
+#include "Error.h"
+#include "environ.h"
+
+#include "dsp/Transformation.h"
+#include "dsp/TimeSeries.h"
+#include "dsp/Chomper.h"
+
+dsp::Chomper::Chomper() : Transformation<TimeSeries,TimeSeries>("Chomper",inplace){
+  new_ndat = 0;
+  rounding = 1;
+  use_new_ndat = false;
+}
+
+void dsp::Chomper::transformation(){
+  if( use_new_ndat ){
+    if( get_input()->get_ndat() < new_ndat )
+      throw Error(InvalidParam,"dsp::Chomper::transformation()",
+		  "You wanted to chomp the timeseries to ndat="UI64" but it only had "UI64" points!",
+		  new_ndat, get_input()->get_ndat());
+    get_input()->set_ndat( new_ndat );
+  }
+
+  get_input()->set_ndat( get_input()->get_ndat() % rounding );
+}
---
+++ Signal/General/dsp/Chomper.h
@@ -0,0 +1,52 @@
+//-*-C++-*-
+
+#ifndef __Chomper_h
+#define __Chomper_h
+
+#include "environ.h"
+
+#include "dsp/Transformation.h"
+#include "dsp/TimeSeries.h"
+
+namespace dsp {
+
+  class Chomper : public Transformation<TimeSeries,TimeSeries> {
+  public:
+
+    //! Default constructor- always inplace
+    Chomper();
+
+    //! Set the new ndat
+    void set_new_ndat(uint64 _new_ndat){ new_ndat = _new_ndat; use_new_ndat = true; }
+
+    //! Inquire the new ndat
+    uint64 get_new_ndat(){ return new_ndat; }
+
+    //! Set the new rounding factor
+    void set_rounding(uint64 _rounding){ rounding = _rounding; }
+
+    //! Inquire the new rounding factor
+    uint64 get_rounding(){ return rounding; }
+
+    //! Don't chomp off the timeseries to new_ndat
+    void dont_use_new_ndat(){ use_new_ndat = false; }
+
+  protected:
+    void transformation();
+
+  private:
+
+    //! The new ndat for the TimeSeries
+    uint64 new_ndat;
+
+    //! After setting the ndat, it is rounded off to divide this number
+    uint64 rounding;
+
+    //! Gets set to true on set_new_ndat
+    bool use_new_ndat;
+
+  };
+
+}
+
+#endif
