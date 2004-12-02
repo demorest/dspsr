e0222fdfefc2ff22fa069e5450de3357de586340
hknight <hknight>
2004-12-02 03:52:01 +0000

    Skeleton

--- Kernel/Formats/bcpm/BCPMUnpacker.C
+++ Kernel/Formats/bcpm/BCPMUnpacker.C
@@ -1,59 +1,22 @@
+#include "Error.h"
 
-#include "dsp/ACFUnpack.h"
 #include "dsp/TimeSeries.h"
 #include "dsp/BitSeries.h"
 
-#include "fftm.h"
-#include "Error.h"
+#include "dsp/BCPMUnpacker.h"
 
 //! Null constructor
-dsp::ACFUnpack::ACFUnpack (const char* _name) : Unpacker (_name)
-{
+dsp::BCPMUnpacker::BCPMUnpacker (const char* _name) : Unpacker (_name){ }
+
+//! Destructor
+dsp::BCPMUnpacker::~BCPMUnpacker (){ }
+
+bool dsp::BCPMUnpacker::matches (const Observation* observation){
+  return observation->get_machine() == "BCPM";
 }
 
-dsp::ACFUnpack::~ACFUnpack ()
-{
-}
-
-bool dsp::ACFUnpack::matches (const Observation* observation)
-{
-  return observation->get_machine() == "Spigot";
-}
+//! Does the work
+void dsp::BCPMUnpacker::unpack (){
 
 
-//! Initialize and resize the output before calling unpack
-void dsp::ACFUnpack::unpack ()
-{
-  if (verbose)
-    cerr << "dsp::ACFUnpack::unpack" << endl;;
-
-  uint64 ndat = input->get_ndat();
-  unsigned nchan = input->get_nchan();
-
-  const uint16* input16 = reinterpret_cast<const uint16*>(input->get_rawptr());
-
-  float* input_fft = float_workingspace (nchan * 4 + 2);
-  float* output_fft = input_fft + nchan * 2;
-
-  unsigned ichan;
-
-  for (unsigned idat=0; idat < ndat; idat++) {
-
-    for (ichan=0; ichan<nchan; ichan++)
-      input_fft[ichan] = (float) input16[ichan];
-
-    input_fft[nchan] = 0.0;
-
-    for (ichan=1; ichan<nchan; ichan++)
-      input_fft[nchan+ichan] = input_fft[nchan-ichan];
-
-    fft::frc1d (nchan*2, output_fft, input_fft);
-
-    for (unsigned ichan=0; ichan<nchan; ichan++)
-      output->get_datptr (ichan) [idat] = output_fft[ichan*2];
-
-  }
-
-  if (verbose)
-    cerr << "dsp::ACFUnpack::unpack exit" << endl;
 }
