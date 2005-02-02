2be46c0912319b76f54a264b131b67ecc42050da
tuppal <tuppal>
2005-02-02 02:52:30 +0000

    registers the EightBitFile.C

--- Kernel/Formats/File_registry.C
+++ Kernel/Formats/File_registry.C
@@ -26,6 +26,12 @@ Registry::List<dsp::File> dsp::File::registry;
 #if DSP_CPSR2
 #include "dsp/CPSR2File.h"
 static Registry::List<dsp::File>::Enter<dsp::CPSR2File> register_cpsr2;
+//#endif
+
+//Tej's new Eight bit File Format:
+//#if DSP_CPSR2
+#include "dsp/EightBitFile.h"
+static Registry::List<dsp::File>::Enter<dsp::EightBitFile> register_eightbitcpsr2;
 #endif
 
 // This is defined in libdsp.a
@@ -41,19 +47,15 @@ static Registry::List<dsp::File>::Enter<dsp::MiniFile> register_minifile;
 static Registry::List<dsp::File>::Enter<dsp::CPSRFile> register_cpsr;
 #endif
 
-#if DSP_Maxim
+/*#if DSP_Maxim
 #include "dsp/MaximFile.h"
 static Registry::List<dsp::File>::Enter<dsp::MaximFile> register_maxim;
 #endif
+*/
 
 #if DSP_SMRO
 #include "dsp/SMROFile.h"
 static Registry::List<dsp::File>::Enter<dsp::SMROFile> register_smro;
-#endif
-
-#if DSP_K5
-#include "dsp/K5File.h"
-static Registry::List<dsp::File>::Enter<dsp::K5File> register_k5;
 #endif
 
 #if DSP_VSIB
@@ -76,12 +78,9 @@ static Registry::List<dsp::File>::Enter<dsp::PuMaFile> register_puma;
 static Registry::List<dsp::File>::Enter<dsp::SpigotFile> register_spigot;
 #endif
 
-#if DSP_BCPM
-#include "dsp/BCPMFile.h"
-static Registry::List<dsp::File>::Enter<dsp::BCPMFile> register_bcpm;
-#endif
-
 // these are defined in libdsp.a
+#include "dsp/CoherentFBFile.h"
+static Registry::List<dsp::File>::Enter<dsp::CoherentFBFile> register_coherentfbfile;
 #include "dsp/BitSeriesFile.h"
 static Registry::List<dsp::File>::Enter<dsp::BitSeriesFile> register_bitseriesfile;
 //#include "dsp/MiniFile.h"
