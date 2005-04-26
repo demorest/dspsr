3b7494d1d9a8f618d5fb8c70f12def34ecab349b
wvanstra <wvanstra>
2005-04-26 13:06:16 +0000

    now inherits BitStatsPlotter

--- Signal/General/TwoBitStatsPlotter.C
+++ Signal/General/TwoBitStatsPlotter.C
@@ -1,3 +1,7 @@
+#include "dsp/TwoBitStatsPlotter.h"
+#include "dsp/TwoBitCorrection.h"
+#include "genutil.h"
+
 #include <iostream>
 #include <algorithm>
 
@@ -6,37 +10,44 @@
 
 #include <cpgplot.h>
 
-#include "dsp/TwoBitStatsPlotter.h"
-#include "dsp/TwoBitCorrection.h"
-#include "genutil.h"
 
 dsp::TwoBitStatsPlotter::TwoBitStatsPlotter ()
 {
   theory_calculated = false;
   theory_max = 0.0;
-  data = 0;
 
   show_cutoff_sigma = true;
   plot_only_range = false;
-  horizontal = true;
-
-  full_xscale = false;
-  hist_min = 0.01;
-
-  vpxmin = 0.1;
-  vpxmax = 0.9;
-  vpymin = 0.1;
-  vpymax = 0.9;
 }
 
 dsp::TwoBitStatsPlotter::~TwoBitStatsPlotter ()
 {
 }
 
-void dsp::TwoBitStatsPlotter::set_data (const TwoBitCorrection* stats)
+string dsp::TwoBitStatsPlotter::get_xlabel () const
 {
-  data = stats;
+    char label[64];
+    sprintf (label, "Low state count (in %d pts)", twobit->get_nsample());
+    return label;
+}
+
+string dsp::TwoBitStatsPlotter::get_ylabel () const
+{
+    return "Number of weights";
+}
+
+
+void dsp::TwoBitStatsPlotter::set_data (const HistUnpacker* hist)
+{
+  const TwoBitCorrection* stats = dynamic_cast<const TwoBitCorrection*>(hist);
+  if (!stats)
+    throw Error (InvalidParam, "dsp::TwoBitStatsPlotter::set_data",
+                 "HistUnpacker is not a TwoBitCorrection");
+
+  twobit = stats;
   theory_calculated = false;
+
+  BitStatsPlotter::set_data (hist);
 }
 
 float gammln(float xx)
@@ -66,12 +77,12 @@ float factln(int n) {
 
 void dsp::TwoBitStatsPlotter::calculate_theory ()
 {
-  if (!data) {
+  if (!twobit) {
     cerr << "TwoBitStatsPlotter::calculate_theory no data" << endl;
     return;
   }
 
-  if (data->get_nsample() < 1) {
+  if (twobit->get_nsample() < 1) {
     cerr << "TwoBitStatsPlotter::calculate_theory invalid data";
     return;
   }
@@ -80,7 +91,7 @@ void dsp::TwoBitStatsPlotter::calculate_theory ()
     return;
 
   // the number of samples per statistical measure
-  int L = data->get_nsample();
+  int L = twobit->get_nsample();
 
   theory.resize (L);
   theory_max = 0.0;
@@ -89,7 +100,7 @@ void dsp::TwoBitStatsPlotter::calculate_theory ()
 
   double value = 0.0;
   double fraction_of_samples = 0.0;
-  double fraction_ones = data->get_fraction_low();
+  double fraction_ones = twobit->get_fraction_low();
 
   for (int wt=0; wt<L; wt++) {
 
@@ -111,17 +122,17 @@ void dsp::TwoBitStatsPlotter::calculate_theory ()
 
 double dsp::TwoBitStatsPlotter::get_chi_squared (int idig)
 {
-  if (!data)
+  if (!twobit)
     return 0;
 
   calculate_theory ();
 
   // get the histogram for this digitizer
-  data->get_histogram (histogram, idig);
+  twobit->get_histogram (histogram, idig);
 
   // the theoretical binomial distribution must be scaled to the
   // number of weights tested
-  double nweights = data->get_histogram_total (idig);
+  double nweights = twobit->get_histogram_total (idig);
 
   double chisq = 0;
   for (unsigned iwt=0; iwt<histogram.size(); iwt++) {
@@ -133,15 +144,7 @@ double dsp::TwoBitStatsPlotter::get_chi_squared (int idig)
   return chisq;
 }
 
-// helper function
-void cpgpt (vector<float>& vals, int type)
-{
-  for (unsigned i=0; i<vals.size(); i++) {
-    float ind = i;
-    float val = vals[i];
-    cpgpt (1, &ind, &val, type);
-  }
-}
+
 
 void dsp::TwoBitStatsPlotter::set_theory_colour ()
 {
@@ -154,211 +157,34 @@ void dsp::TwoBitStatsPlotter::set_theory_colour ()
     }
 }
 
-void dsp::TwoBitStatsPlotter::set_colours (const vector<int>& ucolours) 
-{
-  colours = ucolours;
-  check_colours();
-  set_theory_colour();
-}
 
 void dsp::TwoBitStatsPlotter::check_colours ()
 {
-  if (!data)
-    return;
-
-  int defc[2] = { 10, 13 };
-
-  int ndig = data->get_ndig ();
-  if (ndig < 0)
-    return;
-
-  if (colours.size() < (unsigned)ndig) {
-    int available = colours.size();
-    colours.resize(ndig);
-
-    if (available == 0)
-      for (unsigned ic=0; ic<colours.size(); ic++)
-	colours[ic] = defc[ic%2];
-
-    else
-      for (unsigned ic=available; ic<colours.size(); ic++)
-	colours[ic] = colours[ic%available];
-
+    BitStatsPlotter::check_colours ();
     set_theory_colour();
-  }
 }
 
-void dsp::TwoBitStatsPlotter::pglabel()
-{
-  if (!data)
-    return;
 
-  check_colours ();
-  cpgsci (1);
-  cpgsch (0.75);
 
-  int nsample = data->get_nsample();
-  if (nsample < 0)
-    return;
 
-  int ndig = data->get_ndig ();
-  if (ndig < 0)
-    return;
-
-  char label [80];
-  sprintf (label, "Number of ones (in %d pts)", nsample );
-  cpglab (label, "Number of weights", " ");
-
-  if (ndig == 4) {
-    cpgsci (colours[0]);
-    cpgmtxt("T", .5, 0.0, 0.0, "In-phase");
-    cpgsci (colours[1]);
-    cpgmtxt("T", .5, 1.0, 1.0, "Quadrature");
-    if (colours[0] != colours[2]) {
-      cpgsci (1);
-      cpgmtxt("T", .5, 0.5, 0.5, "Left");
-      cpgmtxt("T", 1.5, 0.5, 0.5, "Right");
-
-      cpgsci (colours[2]);
-      cpgmtxt("T", 1.5, 0.0, 0.0, "In-phase");
-      cpgsci (colours[3]);
-      cpgmtxt("T", 1.5, 1.0, 1.0, "Quadrature");
-    }
-  }
-  // not necessary?
-  //  cpgsci (theory_colour);
-  //  cpgmtxt("T", .5, 0.5, 0.5, "theory");
-}
-
-void dsp::TwoBitStatsPlotter::set_viewport (float _vpxmin, float _vpxmax,
-					    float _vpymin, float _vpymax)
-{
-  vpxmin = _vpxmin;
-  vpxmax = _vpxmax;
-  vpymin = _vpymin;
-  vpymax = _vpymax;
-}
-
-void dsp::TwoBitStatsPlotter::plot ()
-{
-  if (!data)
-    return;
-
-  check_colours ();
-
-  int nsample = data->get_nsample();
-  if (nsample < 0)
-    return;
-
-  int ndig = data->get_ndig ();
-  if (ndig < 0)
-    return;
-
-  float x1=vpxmax;
-  float x2=vpxmin;
-  float y1=vpymax;
-  float y2=vpymin;
-
-  float adjust;
-
-  if (ndig > 1)
-    if (horizontal)
-      x1 = x2 = (vpxmax + vpxmin) / 2.0;
-    else {
-      y1 = y2 = (vpymax + vpymin) / 2.0;
-      adjust = (vpymax - vpymin) * .025;
-      y1 -= adjust;
-      y2 += adjust;
-    }
-
-  // plot the labels
-  cpgsvp (vpxmin, vpxmax, vpymin, vpymax);
-  pglabel ();
-  
-  cpgsvp (vpxmin, x1, y2, vpymax);
-  pgplot (0);
-  cpgsci (1);
-  cpgsch (0.8);
-  cpgmtxt("T", -1.5, 0.05, 0.0, "Left Polarization");
-  
-  cpgsvp (x2, vpxmax, vpymin, y1);
-  pgplot (1);
-  cpgsci (1);
-  cpgsch (0.8);
-  cpgmtxt("T", -1.5, 0.05, 0.0, "Right Polarization");
-}
-
-void dsp::TwoBitStatsPlotter::pgplot (int poln)
-{
-  if (!data)
-    return;
-
-  check_colours ();
-
-  int nsample = data->get_nsample();
-  if (nsample < 0)
-    return;
-
-  int ndig = data->get_ndig ();
-  if (ndig < 0)
-    return;
-  
-  int istat = 0;
-  int iendt = 0;
-
-  if (poln == 4)
-    iendt = ndig -1;
-  else {
-    istat = poln * (ndig / 2);
-    if (ndig == 4)
-      iendt = istat + 1;
-    else
-      iendt = istat;
-  }
-
-  // find the range of the data to be plotted
-  vector<float>::iterator maxel;
-  float ymax = 0.0;
-  int imax=0, imin=nsample-1;
-
-  int idig = 0;
-  for (idig=istat; idig <= iendt; idig++) {
-
-    data->get_histogram (histogram, idig);
-
-    maxel = max_element(histogram.begin(), histogram.end());
-    
-    if (maxel == histogram.end())
-      throw_str ("TwoBitStatsPlotter::pgplot: empty range idig=%d", idig);
-    
-    ymax = max (ymax, *maxel);
-    
-    if (!full_xscale) {
-      int imin_orig = imin;
-      for (imin=0; imin<imin_orig; imin++)
-	if (histogram[imin] > ymax * hist_min)
-	  break;
-      int imax_orig = imax;
-      for (imax=nsample-1; imax>imax_orig; imax--)
-	if (histogram[imax] > ymax * hist_min)
-	  break;
-    }
-  }
-  
+bool dsp::TwoBitStatsPlotter::special (unsigned imin, unsigned imax,
+				       float& ymax)
+{  
   // calculate the theortical distribution of 1-count histogram
   calculate_theory ();
   
   // the theoretical binomial distribution must be scaled to the
   // number of weights tested
-  double nweights = data->get_histogram_total (0);
-  
+  double nweights = twobit->get_histogram_total (0);
+  unsigned nsample = twobit->get_nsample ();
+
   vector<float> plot_theory (nsample);
-  for (int iwt=0; iwt<nsample; iwt++)
+  for (unsigned iwt=0; iwt<nsample; iwt++)
     plot_theory[iwt] = theory[iwt] * nweights;
   
   float hp_min, hp_max;
-  int n_min = data->get_nmin ();
-  int n_max = data->get_nmax ();
+  unsigned n_min = twobit->get_nmin ();
+  unsigned n_max = twobit->get_nmax ();
 
   if (plot_only_range)  {
     hp_min = n_min - 10;
@@ -377,9 +203,9 @@ void dsp::TwoBitStatsPlotter::pgplot (int poln)
 
   // adjust to keep theoretical values in plot
   if (hp_min < imin)
-    imin = (int) hp_min;
+    imin = (unsigned) hp_min;
   if (hp_max > imax)
-    imax = (int) hp_max;
+    imax = (unsigned) hp_max;
 
   // want to keep the entire theoretical curve in the box
   if (ymax < theory_max * nweights)
@@ -392,11 +218,7 @@ void dsp::TwoBitStatsPlotter::pgplot (int poln)
   fprintf (stderr, "histogram (%d->%d) ymax: %f\n", imin, imax, ymax);
 #endif
 
-  cpgsch(0.5);
   cpgswin (imin, imax, ymax*hist_min, ymax);
-  cpgsci (1);
-  cpgsls (1);
-  cpgbox  ("bcnst",0.0,0,"bcnvst",0.0,0);
 
   // plot the theoretical distribution of number of ones
   cpgsci(theory_colour);
@@ -410,22 +232,5 @@ void dsp::TwoBitStatsPlotter::pgplot (int poln)
     cpgdraw (n_max, theory_max * nweights);
   }
 
-  // plot the actual distribution of number of ones
-  float midheight = ymax/2.0;
-
-  for (idig=istat; idig <= iendt; idig++) {
-
-    data->get_histogram (histogram, idig);
-
-    cpgsci (colours[idig]);
-    cpgpt (histogram, 2);
-    
-    float fractone = data->get_histogram_mean (idig);
-    cpgsls (4);
-    cpgmove (fractone*float(nsample), 0.0);
-    cpgdraw (fractone*float(nsample), midheight);
-
-  }
-
-  cpgsls (1);
+  return true;
 }
--- Signal/General/dsp/TwoBitStatsPlotter.h
+++ Signal/General/dsp/TwoBitStatsPlotter.h
@@ -1,23 +1,21 @@
 //-*-C++-*-
 
 /* $Source: /cvsroot/dspsr/dspsr/Signal/General/dsp/Attic/TwoBitStatsPlotter.h,v $
-   $Revision: 1.7 $
-   $Date: 2004/11/22 22:55:36 $
+   $Revision: 1.8 $
+   $Date: 2005/04/26 13:06:16 $
    $Author: wvanstra $ */
 
 #ifndef __TwoBitStatsPlotter_h
 #define __TwoBitStatsPlotter_h
 
-#include <vector>
-
-#include "Reference.h"
+#include "dsp/BitStatsPlotter.h"
 
 namespace dsp {
   
   class TwoBitCorrection;
 
   //! Plots the histograms maintained by TwoBitCorrection
-  class TwoBitStatsPlotter : public Reference::Able {
+  class TwoBitStatsPlotter : public BitStatsPlotter {
 
   public:
 
@@ -28,19 +26,16 @@ namespace dsp {
     virtual ~TwoBitStatsPlotter ();
 
     //! Set the data to be plotted
-    void set_data (const TwoBitCorrection* stats);
+    void set_data (const HistUnpacker* stats);
 
-    //! Set the device-normalized coordinates of viewport frame
-    void set_viewport (float xmin, float xmax, float ymin, float ymax);
+    //! The label on the x-axis
+    std::string get_xlabel () const;
 
-    //! Set the plot colour used for each digitizer
-    void set_colours (const std::vector<int>& colours);
+    //! The label on the y-axis
+    std::string get_ylabel () const;
 
-    //! Get the colour used to plot the theoretical distribution
+     //! Get the colour used to plot the theoretical distribution
     int get_theory_colour () { return theory_colour; };
-
-    //! Plot the data in the currently open viewport
-    void plot ();
 
     //! Get a measure of the difference between the histogram and the theory
     double get_chi_squared (int idig);
@@ -51,19 +46,7 @@ namespace dsp {
     //! Plot vertical bars to indicate the cut-off thresholds
     bool plot_only_range;
 
-   //! Plot the two polarizations beside eachother
-    bool horizontal;
-
-    //! Plot the entire x-axis of the histogram
-    bool full_xscale;
-
-    //! Fraction of histogram maximum below which points are ignored
-    float hist_min;
-
   protected:
-
-    //! Where individual histograms are kept for plotting
-    std::vector<float> histogram;
 
     //! Theoretical, optimal histogram
     std::vector<float> theory;
@@ -77,29 +60,13 @@ namespace dsp {
     //! Colour used when plotting theoretical
     int theory_colour;
 
-    //! Colour used when plotting histogram from each digitizer
-    std::vector <int> colours;
-
-    //! Device normalized coordinates of viewport frame
-    float vpxmin;
-    float vpxmax;
-    float vpymin;
-    float vpymax;
-
     //! Data to be plotted
-    Reference::To<const TwoBitCorrection> data;
+    Reference::To<const TwoBitCorrection> twobit;
 
-    //!
     void calculate_theory ();
-
-    //! Label the plot
-    void pglabel ();
-
-    void pgplot (int poln);
-
     void set_theory_colour ();
-
     void check_colours ();
+    bool special (unsigned imin, unsigned imax, float& ymax);
 
   };
   
