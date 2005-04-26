8f20590ce2bcae5c2bab9ac16dbd11f1f0e33792
wvanstra <wvanstra>
2005-04-26 13:05:32 +0000

    HistUnpacker = Unpacker with histogram

---
+++ Kernel/Classes/HistUnpacker.C
@@ -0,0 +1,90 @@
+// #define _DEBUG 1
+
+#include "dsp/HistUnpacker.h"
+
+#include <iostream>
+
+bool dsp::HistUnpacker::keep_histogram = true;
+
+//! Null constructor
+dsp::HistUnpacker::HistUnpacker (const char* _name) : Unpacker (_name)
+{
+  ndig = 2;
+}
+
+dsp::HistUnpacker::~HistUnpacker ()
+{
+}
+
+/*! By default, there are two digitizers, one for each polarization */
+void dsp::HistUnpacker::set_ndig (unsigned _ndig)
+{
+  if (ndig == _ndig)
+    return;
+
+  if (verbose)
+    cerr << "dsp::HistUnpacker::set_ndig = " << _ndig << endl;
+
+  ndig = _ndig;
+  resize ();
+}
+
+//! Set the number of time samples used to estimate undigitized power
+void dsp::HistUnpacker::set_nsample (unsigned _nsample)
+{
+  if (nsample == _nsample)
+    return;
+
+  if (verbose)
+    cerr << "dsp::HistUnpacker::set_nsample = " << _nsample << endl;
+
+  nsample = _nsample;
+  resize ();
+}
+
+void dsp::HistUnpacker::resize ()
+{
+  histograms.resize (ndig);
+  for (unsigned idig=0; idig<ndig; idig++)
+      histograms[idig].resize (nsample);
+}
+
+
+
+void dsp::HistUnpacker::zero_histogram ()
+{
+  if (verbose)
+    cerr << "dsp::HistUnpacker::zero_histogram" << endl;;
+
+  for (unsigned ichan=0; ichan < histograms.size(); ichan++)
+    for (unsigned ibin=0; ibin<histograms[ichan].size(); ibin++)
+      histograms[ichan][ibin] = 0;
+}
+
+double dsp::HistUnpacker::get_histogram_mean (unsigned idig) const
+{
+  if (idig < 0 || idig >= get_ndig())
+    throw Error (InvalidParam, "dsp::HistUnpacker::get_histogram_mean",
+		 "invalid channel=%d", idig);
+
+  double ones = 0.0;
+  double pts  = 0.0;
+
+  for (unsigned ival=0; ival<nsample; ival++) {
+    double samples = double (histograms[idig][ival]);
+    ones += samples * double (ival);
+    pts  += samples * double (nsample);
+  }
+  return ones/pts;
+}
+
+unsigned long dsp::HistUnpacker::get_histogram_total (unsigned idig) const
+{
+  unsigned long nweights = 0;
+
+  for (unsigned iwt=0; iwt<nsample; iwt++)
+    nweights += histograms[idig][iwt];
+
+  return nweights;
+}
+
---
+++ Kernel/Classes/dsp/HistUnpacker.h
@@ -0,0 +1,86 @@
+//-*-C++-*-
+
+/* $Source: /cvsroot/dspsr/dspsr/Kernel/Classes/dsp/HistUnpacker.h,v $
+   $Revision: 1.1 $
+   $Date: 2005/04/26 13:05:32 $
+   $Author: wvanstra $ */
+
+#ifndef __HistUnpacker_h
+#define __HistUnpacker_h
+
+#include "dsp/Unpacker.h"
+#include <vector>
+
+namespace dsp {
+
+  //! Base class of all unpackers that keep a histogram
+  class HistUnpacker: public Unpacker {
+
+  public:
+
+    //! Maintain a diagnostic histogram of digitizer statistics
+    static bool keep_histogram;
+
+    //! Default constructor
+    HistUnpacker (const char* name = "HistUnpacker");
+
+    //! Virtual destructor
+    virtual ~HistUnpacker ();
+
+    //! Set the number of digitizers (histograms)
+    virtual void set_ndig (unsigned ndig);
+    //! Get the number of digitizers (histograms)
+    unsigned get_ndig () const { return ndig; }
+
+    //! Set the number of samples in the histogram
+    virtual void set_nsample (unsigned nsample);
+    //! Get the number of samples in the histogram
+    unsigned get_nsample () const { return nsample; }
+
+    //! Get the histogram for the given digitizer
+    template <typename T>
+    void get_histogram (vector<T>& data, unsigned idig) const;
+
+    //! Get the centroid of the histogram for the given digitizer
+    double get_histogram_mean (unsigned idig) const;
+
+    //! Get the total number of samples in the histogram
+    unsigned long get_histogram_total (unsigned idig) const;
+
+    //! Reset histogram counts to zero
+    void zero_histogram ();
+
+  protected:
+
+    //! Get the pointer to the histogram array
+    unsigned long* get_histogram (unsigned idig)
+	{ return &histograms[idig][0]; }
+
+  private:
+
+    //! Number of samples in the histogram
+    unsigned nsample;
+
+    //! Number of histograms
+    unsigned ndig;
+
+    //! Histograms of number of ones in nsample points
+    vector< vector< unsigned long > > histograms;
+
+    //! Resize the histograms vector to reflect attributes
+    void resize ();
+
+  };
+  
+}
+
+template <typename T> 
+void dsp::HistUnpacker::get_histogram (vector<T>& data, unsigned idig) const
+{
+  data.resize (nsample);
+
+  for (unsigned i=0; i<nsample; i++)
+    data[i] = T(histograms[idig][i]);
+}
+
+#endif
