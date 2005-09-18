23315352735cee2b390340f1db793744004d4c9d
hknight <hknight>
2005-09-18 07:04:44 +0000

    Completely untested

---
+++ Kernel/Formats/pmdaq/PMDAQ_Extension.C
@@ -0,0 +1,49 @@
+#include <string>
+
+#include "environ.h"
+#include "string_utils.h"
+
+#include "dsp/PMDAQ_Extension.h"
+
+//! Null constructor
+dsp::PMDAQ_Extension::PMDAQ_Extension() : dspExtension("PMDAQ_Extension")
+{
+  chan_begin = 0;
+  chan_end = 99999;
+}
+
+//! Virtual destructor
+dsp::PMDAQ_Extension::~PMDAQ_Extension(){ }
+
+//! Return a new copy-constructed instance identical to this instance
+dsp::dspExtension* dsp::PMDAQ_Extension::clone() const{
+  return new PMDAQ_Extension(*this);
+}
+
+//! Return a new null-constructed instance
+dsp::dspExtension* dsp::PMDAQ_Extension::new_extension() const {
+  return new PMDAQ_Extension;
+}
+
+//! Dump out to a string
+string dsp::PMDAQ_Extension::dump_string() const{
+  string s = "pmdaq_begin_chan\t" + make_string(chan_begin) + "\n";
+  s += "pmdaq_end_chan\t" + make_string(chan_end) + "\n";
+  return s;
+}
+
+//! Copy constructor
+dsp::PMDAQ_Extension::PMDAQ_Extension(const PMDAQ_Extension& p) : dspExtension("PMDAQ_Extension"){
+  operator=(p);
+}
+
+//! Assignment operator
+dsp::PMDAQ_Extension& dsp::PMDAQ_Extension::operator=(const PMDAQ_Extension& p){
+  if( &p == this )
+    return *this;
+
+  chan_begin = p.chan_begin;
+  chan_end = p.chan_end;
+
+  return *this;
+}
---
+++ Kernel/Formats/pmdaq/dsp/PMDAQ_Extension.h
@@ -0,0 +1,53 @@
+//-*-C++-*-
+
+#ifndef __dsp_PMDAQ_Extension_h_
+#define __dsp_PMDAQ_Extension_h_
+
+#include "dsp/Observation.h"
+#include "dsp/dspExtension.h"
+
+namespace dsp {
+
+  class PMDAQ_Extension : public dspExtension {
+
+  public:
+
+    //! Null constructor
+    PMDAQ_Extension();
+
+    //! Copy constructor
+    PMDAQ_Extension(const PMDAQ_Extension& p);
+
+    //! Assignment operator
+    PMDAQ_Extension& operator=(const PMDAQ_Extension& p);
+
+    //! Virtual destructor
+    virtual ~PMDAQ_Extension();
+
+    //! Return a new copy-constructed instance identical to this instance
+    virtual dspExtension* clone() const;
+
+    //! Return a new null-constructed instance
+    virtual dspExtension* new_extension() const;
+
+    //! Dump out to a string
+    virtual string dump_string() const;
+
+    void set_chan_begin(unsigned _chan_begin){ chan_begin = _chan_begin; }
+    unsigned get_chan_begin() const { return chan_begin; }
+    void set_chan_end(unsigned _chan_end){ chan_end = _chan_end; }
+    unsigned get_chan_end() const { return chan_end; }
+
+  private:
+
+    //! First channel for the unpacker to start downloading [0]
+    unsigned chan_begin;
+    
+    //! First channel for the unpacker to not download [99999]
+    unsigned chan_end;
+
+  };
+
+}
+
+#endif
