6b31265411d8f0901c621f57e7840ebfd7328fd6
hknight <hknight>
2005-09-28 03:44:51 +0000

    got compiling

--- Signal/General/DedispersionHistory.C
+++ Signal/General/DedispersionHistory.C
@@ -1,3 +1,10 @@
+#include <string>
+
+#include "string_utils.h"
+#include "psr_cpp.h"
+#include "format_it.h"
+
+#include "dsp/dspExtension.h"
 #include "dsp/DedispersionHistory.h"
 
 //! Null constructor
@@ -26,7 +33,7 @@ dsp::DedispersionHistory::clone() const
 dsp::dspExtension*
 dsp::DedispersionHistory::new_extension() const
 {
-    return new DedipersionHistory;
+    return new DedispersionHistory;
 }
 
 //! Add in a dedispersion operation
@@ -37,5 +44,20 @@ dsp::DedispersionHistory::add(string classname, float dm)
     dms.push_back( dm );
 }
 
+//! Dump out to a string
+string
+dsp::DedispersionHistory::dump_string() const
+{
+  vector<string> lines;
 
+  for( unsigned i=0; i<classes.size(); i++)
+    lines.push_back(classes[i] + " " + make_string(dms[i]) + "\n");
 
+  format_it(lines,3);
+
+  string s;
+  for( unsigned i=0; i<classes.size(); i++)
+    s += lines[i];
+
+  return s;
+}
