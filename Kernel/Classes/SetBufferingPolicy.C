20c2180218da0746adf4b4d3628c76a4ec7e10cd
straten <straten>
2006-11-19 15:39:16 +0000

    use output buffering policy by default

--- Kernel/Classes/SetBufferingPolicy.C
+++ Kernel/Classes/SetBufferingPolicy.C
@@ -1,27 +1,43 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2006 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
 #include "dsp/SetBufferingPolicy.h"
 #include "dsp/TimeSeries.h"
 #include "dsp/InputBuffering.h"
 #include "dsp/OutputBuffering.h"
 
-dsp::SetBufferingPolicy::Policy dsp::SetBufferingPolicy::policy;
+using namespace std;
+
+dsp::SetBufferingPolicy::Policy dsp::SetBufferingPolicy::policy =
+  dsp::SetBufferingPolicy::Output;
 
 void dsp::SetBufferingPolicy::set (TransformationBase* base)
 {
+  //cerr << "dsp::SetBufferingPolicy::set" << endl;
+
   if (policy == Output) {
+
+    //cerr << "dsp::SetBufferingPolicy::set policy == Output" << endl;
 
     HasOutput<TimeSeries>* tr = dynamic_cast<HasOutput<TimeSeries>*>(base);
 
-    if (tr && tr->get_type() != inplace)
-      tr->set_buffering_policy( new OutputBuffering(tr) );
+    if (tr && base->get_type() != inplace)
+      base->set_buffering_policy( new OutputBuffering(tr) );
 
   }
 
   if (policy == Input) {
 
+    //cerr << "dsp::SetBufferingPolicy::set policy == Input" << endl;
+
     HasInput<TimeSeries>* tr = dynamic_cast<HasInput<TimeSeries>*>(base);
 
-    if (tr && tr->get_type() != inplace)
-      tr->set_buffering_policy( new InputBuffering(tr) );
+    if (tr && base->get_type() != inplace)
+      base->set_buffering_policy( new InputBuffering(tr) );
 
   }
 }
