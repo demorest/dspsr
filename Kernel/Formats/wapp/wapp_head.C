00a27525e9d223c94ae2cf92540a55e812ab098a
kstovall <kstovall>
2007-02-01 17:26:45 +0000

    Alternative to YACC and LEX

---
+++ Kernel/Formats/wapp/wapp_head.C
@@ -0,0 +1,45 @@
+#include "wapp_head.h"
+#include "machine_endian.h"
+#include <iostream>
+#include <fcntl.h>
+#include "Error.h"
+
+using namespace std;
+
+void readheader(int fd, WAPP_HEADER* hdr)
+{
+  char c;
+  int bytes_read;
+  for(int i=0;i<7813;++i)
+    if((bytes_read=read(fd,(char *)(&c),sizeof(c)))==-1)
+      throw Error(FailedSys,"readheader","Unable to read from input file");
+  bytes_read=read(fd,(char *)(&(*hdr)),sizeof(*hdr));
+  FromLittleEndian(hdr->header_version);
+  FromLittleEndian(hdr->header_size);
+  FromLittleEndian(hdr->obs_time);
+  FromLittleEndian(hdr->samp_time);
+  FromLittleEndian(hdr->wapp_time);
+  FromLittleEndian(hdr->num_lags);
+  FromLittleEndian(hdr->nifs);
+  FromLittleEndian(hdr->level);
+  FromLittleEndian(hdr->lagformat);
+  FromLittleEndian(hdr->lagtrunc);
+  FromLittleEndian(hdr->cent_freq);
+  FromLittleEndian(hdr->bandwidth);
+  FromLittleEndian(hdr->freqinversion);
+  FromLittleEndian(hdr->src_ra);
+  FromLittleEndian(hdr->src_dec);
+  FromLittleEndian(hdr->start_az);
+  FromLittleEndian(hdr->start_za);
+  FromLittleEndian(hdr->start_ast);
+  FromLittleEndian(hdr->start_lst);
+  FromLittleEndian(hdr->sum);
+  FromLittleEndian(hdr->psr_dm);
+  FromLittleEndian(hdr->dumptime);
+  FromLittleEndian(hdr->nbins);
+  if(hdr->header_version!=7)
+    throw Error(InvalidState,"readheader","Header versions do not match!");
+  if(hdr->header_size!=2440)
+    throw Error(InvalidState,"readheader","Header size incorrect!");
+}
+
