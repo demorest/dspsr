1f9c64b81b63032c3e2a679575606e437b899115
straten <straten>
2007-05-30 07:35:50 +0000

    Predictor/Parameters permit no nonsense related to choosing polyco/psrephem

--- Signal/Pulsar/Fold.C
+++ Signal/Pulsar/Fold.C
@@ -18,7 +18,7 @@
 
 using namespace std;
 
-dsp::Fold::Fold (bool _dont_set_limits) :
+dsp::Fold::Fold () :
   Transformation <TimeSeries, PhaseSeries> ("Fold", outofplace, false) 
 {
   // reducer relies on these defaults being what they are
@@ -37,10 +37,6 @@ dsp::Fold::Fold (bool _dont_set_limits) :
   built = false;
 
   idat_start = ndat_fold = 0;
-
-  dont_set_limits = _dont_set_limits;
-
-  dispersion_measure = -1.0;
 }
 
 //! Makes sure parameters are initialised
@@ -93,44 +89,29 @@ void dsp::Fold::prepare (const Observation* observation)
 
   folding_nbin = 0;
 
-  string jbpulsar = observation->get_source();
+  string pulsar = observation->get_source();
 
   if (!source_name.empty())
-    jbpulsar = source_name;
+    pulsar = source_name;
 
-  if (jbpulsar.length() == 0)
+  if (pulsar.length() == 0)
     throw Error (InvalidParam, "dsp::Fold::prepare", "empty source name");
 
   // Is this a CAL?
   if (observation->get_type() == Signal::PolnCal) {
     double calperiod = 1.0/observation->get_calfreq();
-    set_folding_period (calperiod, jbpulsar);
+    set_folding_period (calperiod);
   }
 
-  if (folding_period > 0 
-      && (folding_period_source==jbpulsar || folding_period_source.empty()) )
-    {
-      if (verbose)
-	cerr << "dsp::Fold::prepare using folding_period="
-	     << folding_period << endl;
+  if (folding_period > 0) {
+    if (verbose)
+      cerr << "dsp::Fold::prepare using folding_period="
+	   << folding_period << endl;
     pulsar_ephemeris = 0;
     folding_predictor = 0;
     built = true;
     return;
   }
-
-  if (jbpulsar[0] != 'J' && jbpulsar.length() > 8)
-    jbpulsar = "J" + jbpulsar;
-  else if (jbpulsar[0] != 'B' && jbpulsar.length() > 6)
-    jbpulsar = "B" + jbpulsar;
-
-  if (verbose)
-    cerr << "dsp::Fold::prepare source=" << jbpulsar << endl;
-
-
-  folding_predictor = choose_predictor (observation->get_start_time(), jbpulsar);
-
-  pulsar_ephemeris = choose_ephemeris (jbpulsar);
 
   if (folding_predictor) {
     if (verbose)
@@ -140,83 +121,34 @@ void dsp::Fold::prepare (const Observation* observation)
   }
 
   if (!pulsar_ephemeris) {
-
-    if (verbose) cerr << "dsp::Fold::prepare generating ephemeris" << endl;
-
-    Reference::To<MatchingEphemeris> mephem;
-    mephem = new MatchingEphemeris (jbpulsar.c_str(), 0);
-    add_pulsar_ephemeris( mephem );
-    pulsar_ephemeris = mephem;
-
-  }
-
-  if (verbose)
-    cerr << "dsp::Fold::prepare pulsar_ephemeris psrname='" 
-	 << pulsar_ephemeris->psrname() << "' dm=" 
-	 << pulsar_ephemeris->get_dm() << endl;
-
-#if 0
-
-  dm = ephemeris.get_dm();
-  if (verbose)
-    cerr << "dsp::Fold::prepare psrephem dm = " << dm << endl;
-
-  TODO - ephemeris and coordinates in archive
-
-  // set the source position
-  raw.coordinates.setRadians (ephemeris.jra(), ephemeris.jdec());
-
-  // No longer using the catalogue!!
-  if (psrstat != NULL) {
     if (verbose)
-      cerr << "Looking up " << info.source << " in catalogue\n";
-
-    // look up the pulsar in the catalogue
-    if (!creadcat (jbpulsar.c_str(), psrstat))  {
-      cerr << "dsp::Fold::prepare error creadcat (" 
-	   << jbpulsar << ")\n";
-      return -1;
-    }
-
-    nspan = (*psrstat)->nspan;
-    ncoef = (*psrstat)->ncoef;
+      cerr << "dsp::Fold::prepare generating ephemeris" << endl;
+    pulsar_ephemeris = new psrephem (pulsar.c_str());
   }
 
-#endif
+  if (verbose)
+    cerr << "dsp::Fold::prepare pulsar_ephemeris name='" 
+	 << pulsar_ephemeris->get_name() << "' dm=" 
+	 << pulsar_ephemeris->get_dispersion_measure() << endl;
 
   if (verbose)
     cerr << "dsp::Fold::prepare creating predictor" << endl;
 
-  if (dispersion_measure > 0.0)
-    const_cast<psrephem*>(pulsar_ephemeris.get())->set_dm(dispersion_measure);
-
   folding_predictor = get_folding_predictor (pulsar_ephemeris, observation);
-
-#if 0
-  doppler = 1.0 + psr_poly->doppler_shift(raw.start_time);
-  
-  if (verbose)
-    cerr << "dsp::Fold::prepare Doppler shift from Pulsar::Predictor:" << doppler << endl;
-
-#endif
 
   built = true;
 } 
 
 Pulsar::Predictor*
-dsp::Fold::get_folding_predictor (const psrephem* pephem,
+dsp::Fold::get_folding_predictor (const Pulsar::Parameters* params,
 				  const Observation* observation)
 {
-  Reference::To<MatchingPolyco> mpoly = new MatchingPolyco;
-  
-  const MatchingEphemeris* mephem = 0;
-  mephem = dynamic_cast<const MatchingEphemeris*>(pephem);
+  const psrephem* pephem = dynamic_cast<const psrephem*>( params );
 
-  if( mephem )
-    mpoly->set_matching_sources( mephem->get_matching_sources() );
-  
-  predictors.push_back( mpoly.get() );
-  
+  if (!pephem)
+    throw Error (InvalidState, "dsp::Fold::get_folding_predictor",
+		 "can prepare only psrephem for now");
+
   MJD time = observation->get_start_time();
 
   Tempo::Predict predict;
@@ -228,74 +160,16 @@ dsp::Fold::get_folding_predictor (const psrephem* pephem,
   // predict.set_frequency ( observation->get_centre_frequency() ); ???
   predict.set_parameters ( *pephem );
 
-  mpoly->polyco::operator=( predict.get_polyco (time, time) );
-
-  return mpoly.release();
+  return new polyco( predict.get_polyco (time, time) );
 }
 
-const Pulsar::Predictor*
-dsp::Fold::choose_predictor (const MJD& time, const string& pulsar)
-{
-  if (verbose) cerr << "dsp::Fold::choose_predictor checking "
-		    << predictors.size()
-		    << " specified predictors" << endl;
 
-  for (unsigned ipoly=0; ipoly<predictors.size(); ipoly++){
-
-    if( verbose )
-      cerr << "dsp::Fold::choose_predictor checking Pulsar::Predictor "
-	   << ipoly << " pulsar='" << pulsar << "'" << endl;
-
-    const MatchingPolyco* mpoly;
-    mpoly = dynamic_cast<const MatchingPolyco*>(predictors[ipoly].ptr());
-
-    if( mpoly && mpoly->matches(time,pulsar) )
-      return predictors[ipoly];
-
-    else try {
-      predictors[ipoly]->phase(time);
-      return predictors[ipoly];
-    }
-    catch (...) {
-    }
-
-  }
-
-  return 0;
-}
-
-const psrephem* dsp::Fold::choose_ephemeris (const string& pulsar)
-{
-  if (verbose) cerr << "dsp::Fold::choose_ephemeris checking "
-		    << ephemerides.size()
-		    << " specified ephemerides" << endl;
-
-  for (unsigned ieph=0; ieph<ephemerides.size(); ieph++) {
-
-    if (verbose) cerr << "dsp::Fold::choose_ephemeris compare " 
-		      << pulsar << " and "
-		      << ephemerides[ieph]->psrname() << endl;
-
-    const MatchingEphemeris* mephem;
-    mephem = dynamic_cast<const MatchingEphemeris*>(ephemerides[ieph].ptr());
-
-    if( mephem && mephem->matches(pulsar) )
-      return ephemerides[ieph];
-    else if (pulsar.find (ephemerides[ieph]->psrname()) != string::npos) {
-      if (verbose)
-	cerr << "PSR: " << pulsar << " matches parfile entry\n";
-      return ephemerides[ieph];
-    }
-  }
-
-  return 0;
-}
 
 
 /*! Unless over-ridden by calling the Fold::set_nbin method, this attribute
   determines the maximum number of pulse phase bins into which input data
   will be folded. */
-unsigned dsp::Fold::maximum_nbin = 8192;
+unsigned dsp::Fold::maximum_nbin = 1024;
 
 /*! The minimum width of each pulse phase bin is specified in units of
   the time resolution of the input TimeSeries. */
@@ -431,7 +305,7 @@ std::string dsp::Fold::get_source_name () const
     return input->get_source();
 
   if (pulsar_ephemeris)
-    return pulsar_ephemeris->psrname();
+    return pulsar_ephemeris->get_name();
 
   return "";
 }
@@ -487,7 +361,7 @@ void dsp::Fold::set_nspan (unsigned _nspan)
   built = false;
 }
 
-void dsp::Fold::set_pulsar_ephemeris (const psrephem* ephemeris)
+void dsp::Fold::set_pulsar_ephemeris (const Pulsar::Parameters* ephemeris)
 {
   if (pulsar_ephemeris == ephemeris)
     return;
@@ -496,7 +370,7 @@ void dsp::Fold::set_pulsar_ephemeris (const psrephem* ephemeris)
   built = false;
 }
 
-const psrephem* dsp::Fold::get_pulsar_ephemeris () const
+const Pulsar::Parameters* dsp::Fold::get_pulsar_ephemeris () const
 {
   return pulsar_ephemeris;
 }
@@ -512,20 +386,6 @@ void dsp::Fold::set_input (const TimeSeries* _input)
 
   if (verbose && weighted_input)
     cerr << "dsp::Fold::set_input input is a WeightedTimeSeries" << endl;
-}
-
-//! Add a phase model with which to choose to fold the data
-void dsp::Fold::add_folding_predictor (const Pulsar::Predictor* folding_predictor)
-{
-  if( folding_predictor )
-    predictors.push_back( folding_predictor );
-}
-
-//! Add an ephemeris with which to choose to create the phase model
-void dsp::Fold::add_pulsar_ephemeris (const psrephem* pulsar_ephemeris)
-{
-  if( pulsar_ephemeris )
-    ephemerides.push_back( pulsar_ephemeris );
 }
 
 void dsp::Fold::transformation ()
@@ -604,18 +464,6 @@ void dsp::Fold::transformation ()
   double sampling_interval = pfold / double(folding_nbin);
   output->set_rate (1.0/sampling_interval);
 
-  if( dispersion_measure > 0.0 )
-    get_output()->set_dispersion_measure( dispersion_measure );
-
-#if 0
-  WvS FIX LATER ?
-  if( archive_filename.size() > 0 )
-    get_output()->set_archive_filename( archive_filename );
-
-  if( archive_filename_extension.size() > 0 )
-    get_output()->set_archive_filename_extension( archive_filename_extension );
-#endif
-
   if( !source_name.empty() )
     get_output()->set_source (source_name);
 
@@ -667,7 +515,8 @@ void dsp::Fold::fold (uint64 nweights,
   // midpoint of the first sample
   double mid_idat_start = double(idat_start) + 0.5;
   // MJD of midpoint of the first sample
-  MJD start_time = get_input()->get_start_time() + mid_idat_start / get_input()->get_rate();
+  MJD start_time = get_input()->get_start_time()
+    + mid_idat_start / get_input()->get_rate();
 
   double phi = get_phi(start_time);
   pfold = get_pfold(start_time);
@@ -805,31 +654,8 @@ double dsp::Fold::get_pfold (const MJD& start_time)
 /*! sets idat_start to zero and ndat_fold to input->get_ndat() */
 void dsp::Fold::set_limits (const Observation* input)
 {
-  if( dont_set_limits )
-    return;
-
   idat_start = 0;
   ndat_fold = input->get_ndat();
-
-  /*
-  if( output->get_integration_length()==0 ){
-    idat_start = 0;
-    return;
-  }
-  
-  double secs_overlap = (end_time - input->get_start_time()).in_seconds();
-  
-  if( secs_overlap < 0 )
-    throw Error(InvalidState,"dsp::Fold::workout_idat_start()",
-		"The latest gulp of data start %f seconds after the current folded data ends.  If this is not a bug you will have to edit the code to fix it.",
-		fabs(secs_overlap) );
-  
-  uint64 samps_overlap = nuint64(secs_overlap * input->get_rate());
-  
-  idat_start = samps_overlap;
-
-  ndat_fold =
-  */
 }
 
 
