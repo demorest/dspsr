0d58504aacd57591cac2adb846fb8e97c5c84782
straten <straten>
2007-05-30 07:35:41 +0000

    dsp::set_verbosity implemented

---
+++ Signal/Pulsar/dsp_verbosity.C
@@ -0,0 +1,20 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "dsp/Observation.h"
+#include "dsp/Operation.h"
+#include "dsp/Shape.h"
+#include "dsp/Archiver.h"
+
+void dsp::set_verbosity (unsigned level)
+{
+  dsp::Observation::verbose = (level >= 3);
+  dsp::Operation::verbose =   (level >= 3);
+  dsp::Shape::verbose =       (level >= 3);
+  dsp::Archiver::verbose =    (level >= 2);
+}
+
