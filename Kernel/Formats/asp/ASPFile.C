53dcf467f46c2bfdd4680c2735ea8e711297118e
straten <straten>
2007-06-06 01:00:58 +0000

    cerr attribute is now mutable

--- Kernel/Formats/asp/ASPFile.C
+++ Kernel/Formats/asp/ASPFile.C
@@ -101,19 +101,19 @@ bool dsp::ASPFile::is_valid (const char* filename, int) const
 
   if (header.band_dir != 1 && header.band_dir != -1) {
     if (verbose)
-      std::cerr << "dsp::ASPFile invalid band_dir=" << header.band_dir << endl;
+      cerr << "dsp::ASPFile invalid band_dir=" << header.band_dir << endl;
     return false;
   }
 
   if (fabs(header.ch_bw) > 512.0) {
     if (verbose)
-      std::cerr << "dsp::ASPFile invalid ch_bw=" << header.ch_bw << endl;
+      cerr << "dsp::ASPFile invalid ch_bw=" << header.ch_bw << endl;
     return false;
   }
  
   if (fabs(header.rf) > 12e4) {
     if (verbose)
-      std::cerr << "dsp::ASPFile invalid rf=" << header.rf << endl;
+      cerr << "dsp::ASPFile invalid rf=" << header.rf << endl;
     return false;
   }
 
