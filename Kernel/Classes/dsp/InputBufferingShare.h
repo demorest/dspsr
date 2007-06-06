f8e356961d3ff552a5e27b81a888af51e46847c5
straten <straten>
2007-06-06 01:01:53 +0000

    InputBufferingShare shares an InputBuffering instance between multiple threads

---
+++ Kernel/Classes/dsp/InputBufferingShare.h
@@ -0,0 +1,65 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2007 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#ifndef __InputBufferingShare_h
+#define __InputBufferingShare_h
+
+#include "dsp/InputBuffering.h"
+
+class ThreadContext;
+
+namespace dsp {
+
+  //! Buffers the Transformation input
+  class InputBuffering::Share : public BufferingPolicy {
+
+  public:
+    
+    //! Default constructor
+    Share ();
+
+    //! Constructor
+    Share (InputBuffering*, HasInput<TimeSeries>*);
+
+    //! Destructor
+    ~Share ();
+
+    //! Sort-of clone
+    Share* clone (HasInput<TimeSeries>*);
+
+    //! Perform all buffering tasks required before transformation
+    void pre_transformation ();
+    
+    //! Perform all buffering tasks required after transformation
+    void post_transformation ();
+    
+    //! Set the first sample to be used from the input next time
+    void set_next_start (uint64 next_start_sample);
+    
+    //! Set the minimum number of samples that can be processed
+    void set_minimum_samples (uint64 samples);
+
+  protected:
+    
+    //! The target with input TimeSeries to be buffered
+    HasInput<TimeSeries>* target;
+    
+    //! The shared input buffering policy
+    Reference::To<InputBuffering> buffer;
+    
+    //! Multi-threaded context information
+    ThreadContext* context;
+
+    //! Owner of the context attribute;
+    bool context_owner;
+
+  };
+
+}
+
+#endif // !defined(__InputBuffering_h)
