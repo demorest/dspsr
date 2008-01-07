d1112507c391c910a80170a481cac94ce8b7f7c4
straten <straten>
2008-01-07 04:30:30 +0000

    unload method added

--- Kernel/Classes/ASCIIObservation.C
+++ Kernel/Classes/ASCIIObservation.C
@@ -19,13 +19,13 @@ dsp::ASCIIObservation::ASCIIObservation (const char* header)
   hdr_version = "HDR_VERSION";
 
   if (header)
-    parse (header);
+    load (header);
 }
 
-void dsp::ASCIIObservation::parse (const char* header)
+void dsp::ASCIIObservation::load (const char* header)
 {
   if (header == NULL)
-    throw Error (InvalidState, "ASCIIObservation", "no header!");
+    throw Error (InvalidState, "ASCIIObservation::load", "no header!");
 
   // //////////////////////////////////////////////////////////////////////
   //
@@ -52,7 +52,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   //
   char buffer[64];
   if (ascii_header_get (header, "TELESCOPE", "%s", buffer) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read TELESCOPE");
+    throw Error (InvalidState, "ASCIIObservation", "failed load TELESCOPE");
   
   set_telescope_code( Tempo::code(buffer) );
 
@@ -61,7 +61,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   // SOURCE
   //
   if (ascii_header_get (header, "SOURCE", "%s", buffer) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read SOURCE");
+    throw Error (InvalidState, "ASCIIObservation", "failed load SOURCE");
 
   set_source (buffer);
 
@@ -88,7 +88,7 @@ void dsp::ASCIIObservation::parse (const char* header)
 
     double calfreq;
     if (ascii_header_get (header, "CALFREQ", "%lf", &calfreq) < 0)
-      throw Error (InvalidState, "ASCIIObservation", "failed read FREQ");
+      throw Error (InvalidState, "ASCIIObservation", "failed load FREQ");
     set_calfreq(calfreq);
     cerr << "Set CALFREQ to " << get_calfreq() << endl;
 
@@ -100,7 +100,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   //
   double freq;
   if (ascii_header_get (header, "FREQ", "%lf", &freq) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read FREQ");
+    throw Error (InvalidState, "ASCIIObservation", "failed load FREQ");
 
   set_centre_frequency (freq);
 
@@ -110,7 +110,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   //
   double bw;
   if (ascii_header_get (header, "BW", "%lf", &bw) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read BW");
+    throw Error (InvalidState, "ASCIIObservation", "failed load BW");
 
   set_bandwidth (bw);
 
@@ -130,7 +130,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   //
   int scan_npol;
   if (ascii_header_get (header, "NPOL", "%d", &scan_npol) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read NPOL");
+    throw Error (InvalidState, "ASCIIObservation", "failed load NPOL");
 
   set_npol (scan_npol);
 
@@ -140,7 +140,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   //
   int scan_nbit;
   if (ascii_header_get (header, "NBIT", "%d", &scan_nbit) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read NBIT");
+    throw Error (InvalidState, "ASCIIObservation", "failed load NBIT");
 
   set_nbit (scan_nbit);
 
@@ -150,7 +150,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   //
   int scan_ndim;
   if (ascii_header_get (header, "NDIM", "%d", &scan_ndim) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read NDIM");
+    throw Error (InvalidState, "ASCIIObservation", "failed load NDIM");
 
   set_ndim(scan_ndim);
 
@@ -176,7 +176,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   //
   double sampling_interval;
   if (ascii_header_get (header, "TSAMP", "%lf", &sampling_interval)<0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read TSAMP");
+    throw Error (InvalidState, "ASCIIObservation", "failed load TSAMP");
 
   /* IMPORTANT: TSAMP is the sampling period in microseconds */
   sampling_interval *= 1e-6;
@@ -188,7 +188,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   // UTC_START
   //
   if (ascii_header_get (header, "UTC_START", "%s", buffer) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read UTC_START");
+    throw Error (InvalidState, "ASCIIObservation", "failed load UTC_START");
 
   struct tm utc;
   if (strptime (buffer, "%Y-%m-%d-%H:%M:%S", &utc) == NULL)
@@ -210,7 +210,7 @@ void dsp::ASCIIObservation::parse (const char* header)
   offset_bytes = 0;
   if (ascii_header_get (header, "OBS_OFFSET", UI64, &offset_bytes) < 0 &&
       ascii_header_get (header, "OFFSET", UI64, &offset_bytes) < 0)
-    throw Error (InvalidState, "ASCIIObservation", "failed read OBS_OFFSET");
+    throw Error (InvalidState, "ASCIIObservation", "failed load OBS_OFFSET");
 
   // //////////////////////////////////////////////////////////////////////
   //
@@ -258,5 +258,153 @@ void dsp::ASCIIObservation::parse (const char* header)
     ra = dec = 0.0;
 
   coordinates.setRadians (ra, dec);
+}
+
+/* ***********************************************************************
+ *
+ *
+ *
+ * *********************************************************************** */
+
+void dsp::ASCIIObservation::unload (char* header)
+{
+  if (header == NULL)
+    throw Error (InvalidState, "ASCIIObservation::unload", "no header!");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // HDR_VERSION
+  //
+  float version;
+  if (ascii_header_set (header, hdr_version.c_str(), "%f", version) < 0)
+    cerr << "ASCIIObservation: failed unload " << hdr_version << endl;
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // TELESCOPE
+  //
+  if (ascii_header_set (header, "TELESCOPE", "%c", get_telescope_code() ) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload TELESCOPE");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // SOURCE
+  //
+  if (ascii_header_set (header, "SOURCE", "%s", get_source().c_str()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload SOURCE");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // MODE
+  //
+  string mode;
+  switch (get_type())
+  {
+  case Signal::Pulsar: mode = "PSR"; break;
+  case Signal::PolnCal: mode = "CAL"; break;
+  default: mode = "UNKNOWN"; break;
+  }
+  ascii_header_set (header, "MODE", "%s", mode.c_str());
+    
+
+  if (get_type() == Signal::PolnCal
+      && ascii_header_set (header, "CALFREQ", "%lf", get_calfreq()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload FREQ");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // FREQ
+  //
+  if (ascii_header_set (header, "FREQ", "%lf", get_centre_frequency()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload FREQ");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // BW
+  //
+  if (ascii_header_set (header, "BW", "%lf", get_bandwidth()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload BW");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // NCHAN
+  //
+  if (ascii_header_set (header, "NCHAN", "%d", get_nchan()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload NCHAN");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // NPOL
+  //
+  if (ascii_header_set (header, "NPOL", "%d", get_npol()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload NPOL");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // NBIT
+  //
+  if (ascii_header_set (header, "NBIT", "%d", get_nbit()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload NBIT");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // NDIM
+  //
+  if (ascii_header_set (header, "NDIM", "%d", get_ndim()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload NDIM");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // TSAMP
+  //
+  /* IMPORTANT: TSAMP is the sampling period in microseconds */
+
+  double sampling_interval = 1e6 / get_rate();
+  if (ascii_header_set (header, "TSAMP", "%lf", sampling_interval)<0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload TSAMP");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // UTC_START
+  //
+  MJD epoch = get_start_time();
+  MJD integer_second ( epoch.intday(), epoch.get_secs(), 0.0 );
+
+  char datestr [64];
+  integer_second.datestr( datestr, 64, "%Y-%m-%d-%H:%M:%S" );
+
+  if (ascii_header_set (header, "UTC_START", "%s", datestr) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload UTC_START");
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // OBS_OFFSET
+  //
+  double offset_samples = epoch.get_fracsec() * get_rate();
+  uint64 offset_bytes = get_nbytes( (uint64)offset_samples );
+
+  if (ascii_header_set (header, "OBS_OFFSET", UI64, offset_bytes) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload OBS_OFFSET");
+
+
+  // //////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT
+  //
+  if (ascii_header_set (header, "INSTRUMENT", "%s", get_machine().c_str()) < 0)
+    throw Error (InvalidState, "ASCIIObservation", "failed unload INSTRUMENT");
+
 }
 
