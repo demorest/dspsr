02d7af6862a3fa2eea2b4951d73f444ac6e3281d
straten <straten>
2008-02-20 21:44:11 +0000

    inherit BitTable

--- Kernel/Classes/dsp/TwoBitTable.h
+++ Kernel/Classes/dsp/TwoBitTable.h
@@ -1,60 +1,33 @@
 //-*-C++-*-
 /***************************************************************************
  *
- *   Copyright (C) 2002 by Willem van Straten
+ *   Copyright (C) 2008 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
 
 /* $Source: /cvsroot/dspsr/dspsr/Kernel/Classes/dsp/TwoBitTable.h,v $
-   $Revision: 1.11 $
-   $Date: 2008/02/18 13:27:20 $
+   $Revision: 1.12 $
+   $Date: 2008/02/20 21:44:11 $
    $Author: straten $ */
 
 
 #ifndef __TwoBitTable_h
 #define __TwoBitTable_h
 
-#include "Reference.h"
+#include "dsp/BitTable.h"
 
 namespace dsp {
 
-  //! Look-up table for conversion from two-bit to floating point numbers
-  /*! 
-    The TwoBitTable class can build a lookup up table of four floating point
-    numbers per one-byte character.
-   */
-  class TwoBitTable : public Reference::Able {
+  //! Look-up table for converting 2-bit digitized to floating point numbers
+  class TwoBitTable : public BitTable
+  {
 
   public:
-
-    enum Type { Unset, 
-		OffsetBinary, 
-		SignMagnitude,
-		TwosComplement 
-    };
-
-    //! Number of unique 8-bit combinations
-    static const unsigned unique_bytes;
-
-    //! Number of 2-bit values per byte
-    static const unsigned vals_per_byte;
-
-    //! Number of unique 2-bit values
-    static const unsigned unique_vals;
 
     //! Constructor
     TwoBitTable (Type type);
     
-    //! Destructor
-    virtual ~TwoBitTable ();
-
-    //! Build a two-bit table with the current attributes
-    void build ();
-
-    //! Returns pointer to the four floating-point values represented by byte
-    const float* get_four_vals (unsigned byte);
-
     //! Set the value of the low voltage state
     void set_lo_val (float lo_val);
 
@@ -67,51 +40,28 @@ namespace dsp {
     //! Return the value of the high voltage state
     float get_hi_val () const { return hi_val; }
 
-    //! Set the digitization convention
-    void set_type (Type type);
-
-    //! Return the digitization convention
-    Type get_type () const { return type; }
-
     //! Set the flip value to be true or false
     void set_flip (bool flipped);
 
     //! Get the flip value
     bool get_flip () const { return flip; }
 
-    //! Generate a look-up table for byte to 4xfloating point conversion
-    void generate (float* table) const;
+    //! Build a two-bit table with the current attributes
+    void rebuild ();
 
     //! Generate a look-up table for 2-bit to floating point conversion
-    void four_vals (float* vals) const;
-
-    //! Extract from byte the 2-bit number corresponding to sample
-    virtual unsigned twobit (unsigned byte, unsigned sample) const;
+    void generate_unique_values (float* vals) const;
 
   protected:
 
-    //! For each unsigned char, values of the four output voltage states
-    float* table;
-
-    //! Flag that the lookup table has been built for specified attributes
-    bool built;
-    
     //! Value of voltage in low state
     float lo_val;
 
     //! Value of voltage in high state
     float hi_val;
 
-    //! Digitization convention
-    Type type;
-
     //! Flip the two data bits - that is SignMag becomes MagSign
     bool flip;
-
-  private:
-
-    //! Private default constructor ensures that type is specified
-    TwoBitTable () { }
 
   };
 
