b97943772cd91b7b7b1c7fe7e4161a6231048031
straten <straten>
2008-02-20 21:44:03 +0000

    inherit the EightBitUnpacker class

--- Kernel/Formats/apsr/dsp/APSREightBit.h
+++ Kernel/Formats/apsr/dsp/APSREightBit.h
@@ -6,40 +6,26 @@
  *
  ***************************************************************************/
 
-/* $Source: /cvsroot/dspsr/dspsr/Kernel/Formats/apsr/dsp/APSREightBit.h,v $
-   $Revision: 1.1 $
-   $Date: 2008/02/18 11:04:37 $
-   $Author: straten $ */
-
 #ifndef __APSREightBit_h
 #define __APSREightBit_h
 
-#include "dsp/HistUnpacker.h"
+#include "dsp/EightBitUnpacker.h"
 
 namespace dsp {
 
-  //! Simple 8-bit to float unpacker for APSR files
-  class APSREightBit : public HistUnpacker
-  {
+  //! Converts APSR data from 4-bit digitized to floating point values
+  class APSREightBit: public EightBitUnpacker {
 
   public:
-    
-    //! Constructor
-    APSREightBit (const char* name = "APSREightBit");
 
-   protected:
-    
-    //! The unpacking routine
-    virtual void unpack ();
+    //! Constructor initializes base class attributes
+    APSREightBit ();
 
-    //! Return true if we can convert the Observation
+    //! Return true if APSREightBit can convert the Observation
     virtual bool matches (const Observation* observation);
 
-    unsigned get_output_offset (unsigned idig) const;
-    unsigned get_output_ipol (unsigned idig) const;
-
   };
-
+  
 }
 
-#endif // !defined(__APSREightBit_h)
+#endif
