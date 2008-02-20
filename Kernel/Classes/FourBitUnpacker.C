2a9c1e9b81afb90781fa33d601e35e3bb938aafc
straten <straten>
2008-02-20 21:44:15 +0000

    real and imaginary parts are interleaved

--- Kernel/Classes/FourBitUnpacker.C
+++ Kernel/Classes/FourBitUnpacker.C
@@ -17,7 +17,6 @@ dsp::FourBitUnpacker::FourBitUnpacker (const char* _name)
   : HistUnpacker (_name)
 {
   set_nsample (256);
-  set_ndig (4);
 }
 
 dsp::FourBitUnpacker::~FourBitUnpacker ()
@@ -26,9 +25,6 @@ dsp::FourBitUnpacker::~FourBitUnpacker ()
 
 void dsp::FourBitUnpacker::set_table (FourBitTable* _table)
 {
-  if (table.get() == _table)
-    return;
-
   if (verbose)
     cerr << "dsp::FourBitUnpacker::set_table" << endl;
 
@@ -43,12 +39,13 @@ const dsp::FourBitTable* dsp::FourBitUnpacker::get_table () const
 void dsp::FourBitUnpacker::unpack ()
 {
   const uint64   ndat  = input->get_ndat();
-  const unsigned npol  = input->get_npol();
+
   const unsigned nchan = input->get_nchan();
+  const unsigned npol  = input->get_npol();
   const unsigned ndim  = input->get_ndim();
+
   const unsigned nskip = npol * nchan * ndim;
 
-  //cerr << "npol=" << npol << " ndim=" << ndim << endl;
   const uint64 ndat2 = ndat/2;
   if (ndat % 2)
     throw Error (InvalidParam, "dsp::FourBitUnpacker::unpack",
@@ -56,7 +53,7 @@ void dsp::FourBitUnpacker::unpack ()
 		 
   unsigned offset = 0;
 
-  const float* lookup = table->get_two_vals ();
+  const float* lookup = table->get_values ();
 
   for (unsigned ichan=0; ichan<nchan; ichan++)
   {
@@ -68,15 +65,17 @@ void dsp::FourBitUnpacker::unpack ()
 	float* into = output->get_datptr (ichan, ipol) + idim;
 	unsigned long* hist = get_histogram (offset);
   
+	offset ++;
+
 	for (uint64 idat = 0; idat < ndat2; idat++)
 	{
-	  into[0] = lookup[ *from * 2 ];
-	  into[1] = lookup[ *from * 2 + 1 ];
+	  into[0]    = lookup[ *from * 2 ];
+	  into[ndim] = lookup[ *from * 2 + 1 ];
 
 	  hist[ *from ] ++;
 
 	  from += nskip;
-	  into += ndim;
+	  into += ndim * 2;
 	}
       }
     }
