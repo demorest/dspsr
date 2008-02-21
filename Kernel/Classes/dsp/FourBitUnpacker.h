065e172d206300abd9bd78516c75a70c4afb59c0
straten <straten>
2008-02-21 04:55:58 +0000

    inherit BitUnpacker

--- Kernel/Classes/dsp/FourBitUnpacker.h
+++ Kernel/Classes/dsp/FourBitUnpacker.h
@@ -7,21 +7,19 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/dspsr/dspsr/Kernel/Classes/dsp/FourBitUnpacker.h,v $
-   $Revision: 1.5 $
-   $Date: 2008/02/18 13:27:16 $
+   $Revision: 1.6 $
+   $Date: 2008/02/21 04:55:58 $
    $Author: straten $ */
 
 #ifndef __FourBitUnpacker_h
 #define __FourBitUnpacker_h
 
-#include "dsp/HistUnpacker.h"
+#include "dsp/BitUnpacker.h"
 
 namespace dsp {
 
-  class FourBitTable;
-
   //! Converts 4-bit digitised samples to floating point
-  class FourBitUnpacker: public HistUnpacker
+  class FourBitUnpacker: public BitUnpacker
   {
 
   public:
@@ -29,23 +27,11 @@ namespace dsp {
     //! Null constructor
     FourBitUnpacker (const char* name = "FourBitUnpacker");
 
-    //! Virtual destructor
-    virtual ~FourBitUnpacker ();
-
-    //! Set the digitisation convention
-    void set_table (FourBitTable* table);
-
-    //! Get the digitisation convention
-    const FourBitTable* get_table () const;
-			     
   protected:
 
-    //! The four bit table generator  
-    Reference::To<FourBitTable> table;
-    
-    //! Unpacking algorithm may be re-defined by sub-classes
-    virtual void unpack ();
-	
+    void unpack (uint64 ndat, const unsigned char* from, const unsigned nskip,
+		 float* into, unsigned long* hist);
+
   };
 }
 #endif
