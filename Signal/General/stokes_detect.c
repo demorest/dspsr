05faae06b774997b507bf5984f088d935e48157a
straten <straten>
2008-05-29 10:38:32 +0000

    copyright corrected

--- Signal/General/stokes_detect.c
+++ Signal/General/stokes_detect.c
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- *   Copyright (C) 2004 by wvanstra Willem van Straten
+ *   Copyright (C) 2004 by Willem van Straten
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
