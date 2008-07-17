855faf5b355354bba4a9c5043b40399cc662f745
straten <straten>
2008-07-17 01:17:29 +0000

    mostly implemented by TwoBitLookup

--- Kernel/Classes/TwoBit1or2.C
+++ Kernel/Classes/TwoBit1or2.C
@@ -12,10 +12,7 @@
 
 dsp::TwoBit1or2::TwoBit1or2 ()
 {
-  nlow = nlow_min = nlow_max = 0;
-  lookup_base = 0;
   temp_values = 0;
-  ndim_per_digitizer = 1;
 }
 
 dsp::TwoBit1or2::~TwoBit1or2 ()
@@ -25,20 +22,7 @@ dsp::TwoBit1or2::~TwoBit1or2 ()
 
 void dsp::TwoBit1or2::destroy ()
 {
-  if (lookup_base) delete [] lookup_base; lookup_base = 0;
   if (temp_values) delete [] temp_values; temp_values = 0;
-}
-
-void dsp::TwoBit1or2::set_nlow_min (unsigned min)
-{
-  nlow_min = min;
-  destroy ();
-}
-
-void dsp::TwoBit1or2::set_nlow_max (unsigned max)
-{
-  nlow_max = max;
-  destroy ();
 }
 
 void dsp::TwoBit1or2::nlow_build (TwoBitTable* table)
@@ -57,40 +41,22 @@ void dsp::TwoBit1or2::nlow_build (TwoBitTable* table)
       nlow_lookup[ifv] = 0;
 }
 
-void dsp::TwoBit1or2::lookup_build (unsigned nsamp, unsigned ndim,
-				    TwoBitTable* table,
-				    JenetAnderson98* ja98)
+void dsp::TwoBit1or2::get_lookup_block (float* lookup, TwoBitTable* table)
 {
-  destroy ();
-
-  assert (nlow_max > nlow_min);
-  assert (nsamp > nlow_max);
-
-  lookup_base = new float [(nlow_max - nlow_min + 1) * 4];
-  temp_values = new unsigned char [nsamp * ndim];
-
-  ndim_per_digitizer = ndim;
-
-  float* lookup = lookup_base;
-
-  for (unsigned nlo = nlow_min; nlo <= nlow_max; nlo++)
-  {
-    /* Refering to JA98, nlo is the number of samples between x2 and x4, 
-       and p_in is the left-hand side of Eq.44 */
-
-    float p_in = (float) nlo / (float) nsamp;
-
-    if (ja98)
-    {
-      ja98->set_Phi (p_in);
-      
-      table->set_lo_val ( ja98->get_lo() );
-      table->set_hi_val ( ja98->get_hi() );
-    }
-    
-    // Generate the four output levels corresponding to each 2-bit number
-    table->generate_unique_values ( lookup );
-    lookup += table->get_unique_values();
-  }
+  // Generate the four output levels corresponding to each 2-bit number
+  table->generate_unique_values ( lookup );
 }
 
+unsigned dsp::TwoBit1or2::get_lookup_block_size ()
+{
+  // four unique 2-bit numbers
+  return 4;
+}
+
+void dsp::TwoBit1or2::create ()
+{
+  TwoBitLookup::create ();
+
+  destroy ();
+  lookup_base = new float [(nlow_max - nlow_min + 1) * 4];
+}
