0fa94ad9a7e1a9894b0c7ba3b242e31365cc5bea
straten <straten>
2008-11-06 01:25:21 +0000

    totalbytes accounting doesn't work; use ftell instead

--- Kernel/Formats/sigproc/read_header.c
+++ Kernel/Formats/sigproc/read_header.c
@@ -26,7 +26,7 @@ void get_string(FILE *inputfile, int *nbytes, char string[]) /* includefile */
 int read_header(FILE *inputfile) /* includefile */
 {
   char string[80], message[80];
-  int itmp,nbytes,totalbytes,expecting_rawdatafile=0,expecting_source_name=0; 
+  int itmp,nbytes,expecting_rawdatafile=0,expecting_source_name=0; 
   int expecting_frequency_table=0,channel_index=0;
 
 
@@ -37,14 +37,11 @@ int read_header(FILE *inputfile) /* includefile */
 	rewind(inputfile);
 	return 0;
   }
-  /* store total number of bytes read so far */
-  totalbytes=nbytes;
 
   /* loop over and read remaining header lines until HEADER_END reached */
   while (1) {
     get_string(inputfile,&nbytes,string);
     if (strings_equal(string,"HEADER_END")) break;
-    totalbytes+=nbytes;
     if (strings_equal(string,"rawdatafile")) {
       expecting_rawdatafile=1;
     } else if (strings_equal(string,"source_name")) {
@@ -56,78 +53,54 @@ int read_header(FILE *inputfile) /* includefile */
       expecting_frequency_table=0;
     } else if (strings_equal(string,"az_start")) {
       fread(&az_start,sizeof(az_start),1,inputfile);
-      totalbytes+=sizeof(az_start);
     } else if (strings_equal(string,"za_start")) {
       fread(&za_start,sizeof(za_start),1,inputfile);
-      totalbytes+=sizeof(za_start);
     } else if (strings_equal(string,"src_raj")) {
       fread(&src_raj,sizeof(src_raj),1,inputfile);
-      totalbytes+=sizeof(src_raj);
     } else if (strings_equal(string,"src_dej")) {
       fread(&src_dej,sizeof(src_dej),1,inputfile);
-      totalbytes+=sizeof(src_dej);
     } else if (strings_equal(string,"tstart")) {
       fread(&tstart,sizeof(tstart),1,inputfile);
-      totalbytes+=sizeof(tstart);
     } else if (strings_equal(string,"tsamp")) {
       fread(&tsamp,sizeof(tsamp),1,inputfile);
-      totalbytes+=sizeof(tsamp);
     } else if (strings_equal(string,"period")) {
       fread(&period,sizeof(period),1,inputfile);
-      totalbytes+=sizeof(period);
     } else if (strings_equal(string,"fch1")) {
       fread(&fch1,sizeof(fch1),1,inputfile);
-      totalbytes+=sizeof(fch1);
     } else if (strings_equal(string,"fchannel")) {
       fread(&frequency_table[channel_index++],sizeof(double),1,inputfile);
-      totalbytes+=sizeof(double);
       fch1=foff=0.0; /* set to 0.0 to signify that a table is in use */
     } else if (strings_equal(string,"foff")) {
       fread(&foff,sizeof(foff),1,inputfile);
-      totalbytes+=sizeof(foff);
     } else if (strings_equal(string,"nchans")) {
       fread(&nchans,sizeof(nchans),1,inputfile);
-      totalbytes+=sizeof(nchans);
     } else if (strings_equal(string,"telescope_id")) {
       fread(&telescope_id,sizeof(telescope_id),1,inputfile);
-      totalbytes+=sizeof(telescope_id);
     } else if (strings_equal(string,"machine_id")) {
       fread(&machine_id,sizeof(machine_id),1,inputfile);
-      totalbytes+=sizeof(machine_id);
     } else if (strings_equal(string,"data_type")) {
       fread(&data_type,sizeof(data_type),1,inputfile);
-      totalbytes+=sizeof(data_type);
     } else if (strings_equal(string,"ibeam")) {
       fread(&ibeam,sizeof(ibeam),1,inputfile);
-      totalbytes+=sizeof(ibeam);
     } else if (strings_equal(string,"nbeams")) {
       fread(&nbeams,sizeof(nbeams),1,inputfile);
-      totalbytes+=sizeof(nbeams);
     } else if (strings_equal(string,"nbits")) {
       fread(&nbits,sizeof(nbits),1,inputfile);
-      totalbytes+=sizeof(nbits);
     } else if (strings_equal(string,"barycentric")) {
       fread(&barycentric,sizeof(barycentric),1,inputfile);
-      totalbytes+=sizeof(barycentric);
     } else if (strings_equal(string,"pulsarcentric")) {
       fread(&pulsarcentric,sizeof(pulsarcentric),1,inputfile);
-      totalbytes+=sizeof(pulsarcentric);
     } else if (strings_equal(string,"nbins")) {
       fread(&nbins,sizeof(nbins),1,inputfile);
-      totalbytes+=sizeof(nbins);
     } else if (strings_equal(string,"nsamples")) {
       /* read this one only for backwards compatibility */
       fread(&itmp,sizeof(itmp),1,inputfile);
-      totalbytes+=sizeof(itmp);
     } else if (strings_equal(string,"nifs")) {
       fread(&nifs,sizeof(nifs),1,inputfile);
-      totalbytes+=sizeof(nifs);
     } else if (strings_equal(string,"npuls")) {
       fread(&npuls,sizeof(npuls),1,inputfile);
-      totalbytes+=sizeof(npuls);
     } else if (strings_equal(string,"refdm")) {
       fread(&refdm,sizeof(refdm),1,inputfile);
-      totalbytes+=sizeof(refdm);
     } else if (expecting_rawdatafile) {
       strcpy(rawdatafile,string);
       expecting_rawdatafile=0;
@@ -141,9 +114,7 @@ int read_header(FILE *inputfile) /* includefile */
     } 
   } 
 
-  /* add on last header string */
-  totalbytes+=nbytes;
-
   /* return total number of bytes read */
-  return totalbytes;
+  return ftell(inputfile);
 }
+
