df010f7c541b04a2db6cd9a2bdee90802d75d13e
straten <straten>
2009-05-07 08:08:13 +0000

    attachment to Operations that have input

---
+++ Kernel/Classes/dsp/HasInput.h
@@ -0,0 +1,45 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2009 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/dspsr/dspsr/Kernel/Classes/dsp/HasInput.h,v $
+   $Revision: 1.1 $
+   $Date: 2009/05/07 08:08:13 $
+   $Author: straten $ */
+
+#ifndef __dsp_HasInput_h
+#define __dsp_HasInput_h
+
+#include "ReferenceTo.h"
+
+namespace dsp
+{
+  template <class In>
+  class HasInput
+  {
+  public:
+
+    //! Destructor
+    virtual ~HasInput () {}
+
+    //! Set the container from which input data will be read
+    virtual void set_input (In* _input) { input = _input; }
+
+    //! Return pointer to the container from which input data will be read
+    const In* get_input () const { return input; }
+ 
+    //! Returns true if input is set
+    bool has_input() const { return input; }
+
+  protected:
+
+    //! Container from which input data will be read
+    Reference::To<const In> input;
+  };
+}
+
+#endif
