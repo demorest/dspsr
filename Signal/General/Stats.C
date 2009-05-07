2a32456a42f182605907473939b6941055f010eb
straten <straten>
2009-05-07 08:46:18 +0000

    compute the mean and variance in each frequency channel and polarization

---
+++ Signal/General/Stats.C
@@ -0,0 +1,75 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2009 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "dsp/Stats.h"
+
+dsp::Stats::Stats (const char* name) : Sink<TimeSeries> (name) { }
+    
+dsp::Stats::~Stats ()
+{
+}
+
+//! Returns mean in given chan,pol
+float dsp::Stats::get_mean (unsigned ichan, unsigned ipol)
+{
+  return sum[ichan][ipol] / total;
+}
+
+template<typename T> T sqr (T x) { return x*x; }
+    
+//! Returns standard deviation in given chan,pol
+float dsp::Stats::get_sigma (unsigned ichan, unsigned ipol)
+{
+  return sqrt( sumsq[ichan][ipol] / total - sqr(get_mean(ichan,ipol)) );
+}
+
+//! Adds to the totals
+void dsp::Stats::calculation ()
+{
+  const unsigned nchan = input->get_nchan();
+  const unsigned npol = input->get_npol();
+  const uint64 ndat = input->get_ndat();
+
+  if (sum.size() < nchan)
+    reset ();
+
+  for (unsigned ichan = 0; ichan < nchan; ichan ++)
+    for (unsigned ipol = 0; ipol < npol; ipol++)
+    {
+      const float* data = input->get_datptr (ichan, ipol);
+      for (uint64 idat = 0; idat < ndat; idat++)
+      {
+	sum[ichan][ipol] += data[idat];
+	sumsq[ichan][ipol] += data[idat] * data[idat];
+      }
+    }
+
+  total += ndat;
+}
+
+void dsp::Stats::reset ()
+{
+  const unsigned nchan = input->get_nchan();
+  const unsigned npol = input->get_npol();
+
+  sum.resize (nchan);
+  sumsq.resize (nchan);
+
+  for (unsigned ichan=0; ichan < nchan; ichan++)
+  {
+    sum[ichan].resize (npol);
+    sumsq[ichan].resize (npol);
+
+    for (unsigned ipol=0; ipol < npol; ipol++)
+    {
+      sum[ichan][ipol] = 0;
+      sumsq[ichan][ipol] = 0;
+    }
+  }
+
+  total = 0;
+}
