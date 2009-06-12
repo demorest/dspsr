b1017c44f839d14b0ae113871a6ec039a9ced06b
straten <straten>
2009-06-12 06:19:06 +0000

    new match method added by Ravi and Willem

--- Signal/General/dsp/PolnCalibration.h
+++ Signal/General/dsp/PolnCalibration.h
@@ -6,12 +6,12 @@
  *
  ***************************************************************************/
 
-#ifndef __PolnCalibrator_h
-#define __PolnCalibrator_h
+#ifndef __dsp_PolnCalibration_h
+#define __dsp_PolnCalibration_h
 
 #include "dsp/Response.h"
 #include "Pulsar/CalibratorTypes.h"
-
+#include "Pulsar/PolnCalibrator.h"
 
 namespace dsp {
 
@@ -23,22 +23,35 @@ class PolnCalibration: public Response {
   
     // constructor
     PolnCalibration ( );
-     
+
+    void set_ndat (unsigned _ndat)
+    { ndat = _ndat; } 
+    
+    void set_nchan (unsigned _nchan)
+    { nchan = _nchan;}
+ 
     void set_database_filename (const std::string& name)
     { database_filename = name; }
 
     void set_type ( Pulsar::Calibrator::Type* _type)
     {  type = _type; }
 
-    virtual void match (const Observation* input, unsigned channels=0);
+  void match (const Observation* input, unsigned channels=0);
+  void match (const Response*);
 
     
 
   protected:
+   
+    unsigned ndat;
+  
+    unsigned nchan;
 
     std::string database_filename;
 
     Reference::To<Pulsar::Calibrator::Type> type;
+
+    Reference::To<Pulsar::PolnCalibrator> pcal;
 
 
   };
