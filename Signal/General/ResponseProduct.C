e2c2d32ce6296cf7d2a7361eae85a40f801d75dc
straten <straten>
2009-06-16 06:16:50 +0000

    ignore the component_changed flag for now

--- Signal/General/ResponseProduct.C
+++ Signal/General/ResponseProduct.C
@@ -4,7 +4,10 @@
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
+
 #include "dsp/ResponseProduct.h"
+
+using namespace std;
 
 dsp::ResponseProduct::ResponseProduct ()
 {
@@ -19,6 +22,9 @@ dsp::ResponseProduct::~ResponseProduct ()
 //! Create a product to match the input
 void dsp::ResponseProduct::match (const Observation* obs, unsigned nchan)
 {
+  if (verbose)
+    cerr << "dsp::ResponseProduct::match (const Observation*)" << endl;
+
   for (unsigned iresp=0; iresp < response.size(); iresp++)
     response[iresp]->match (obs, nchan);
 
@@ -28,6 +34,9 @@ void dsp::ResponseProduct::match (const Observation* obs, unsigned nchan)
 //! Create a product to match the response
 void dsp::ResponseProduct::match (const Response* _response)
 {
+  if (verbose)
+    cerr << "dsp::ResponseProduct::match (const Response*)" << endl;
+
   for (unsigned iresp=0; iresp < response.size(); iresp++)
     response[iresp]->match (_response);
 
@@ -63,32 +72,37 @@ using namespace std;
 
 void dsp::ResponseProduct::build ()
 {
+  if (verbose)
+    cerr << "dsp::ResponseProduct::build" << endl;
+
   if (response.size() == 0)
     throw Error (InvalidState, "dsp::ResponseProduct::build",
 		 "no responses in product");
 
-  if (!component_changed)
-    return;
+  //if (!component_changed)
+  //  return;
 
-  cerr << "ORDER" << endl;
-
-  for (unsigned iresp=0; iresp < response.size(); iresp++)
-    cerr << "ORDER " << iresp << " ndim=" << response[iresp]->get_ndim() << endl;
-
-  cerr  << "MATCHING" << endl;
+  if (verbose)
+    cerr  << "dsp::ResponseProduct::build match_index=" << match_index << endl;
 
   for (unsigned iresp=0; iresp < response.size(); iresp++)
     if (iresp != match_index)
       response[iresp]->match (response[match_index]);
 
-  cerr << "MULTIPLYING" << endl;
+  if (verbose)
+    cerr << "dsp::ResponseProduct::build copy_index=" << copy_index << endl;
 
   Response::operator = (*response[copy_index]);
-  for (unsigned iresp=1; iresp < response.size(); iresp++)
-  {
-    cerr << "RESP=" << iresp << endl;
-    Response::operator *= (*response[iresp]);
-  }
 
-  cerr << "DONE!" << endl;
+  if (verbose)
+    cerr << "dsp::ResponseProduct::build ndat=" << ndat 
+         << " nchan=" << nchan << endl;
+
+  for (unsigned iresp=0; iresp < response.size(); iresp++)
+    if (iresp != copy_index)
+      Response::operator *= (*response[iresp]);
+
+  if (verbose)
+    cerr << "dsp::ResponseProduct::build DONE!" << endl;
 }
+
