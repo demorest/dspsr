e72ddedad6c4867a02cd2745d52f30e1173df48a
straten <straten>
2009-10-29 12:01:05 +0000

    MultiplexUnpacker renamed CASPSRUnpacker

--- Kernel/Formats/caspsr/CASPSRUnpacker.C
+++ Kernel/Formats/caspsr/CASPSRUnpacker.C
@@ -1,6 +1,7 @@
+//-*-C++-*-
 /***************************************************************************
  *
- *   Copyright (C) 2009 by Willem van Straten
+ *   Copyright (C) 2009
  *   Licensed under the Academic Free License version 2.1
  *
  ***************************************************************************/
@@ -8,16 +9,122 @@
 #include "dsp/CASPSRUnpacker.h"
 #include "dsp/BitTable.h"
 
-//! Constructor
-dsp::CASPSRUnpacker::CASPSRUnpacker (const char* name) : EightBitUnpacker (name)
+#include "Error.h"
+
+using namespace std;
+
+dsp::CASPSRUnpacker::CASPSRUnpacker (const char* _name) : HistUnpacker (_name)
 {
+  if (verbose)
+    cerr << "dsp::CASPSRUnpacker ctor" << endl;
+  set_nstate (256);
   table = new BitTable (8, BitTable::TwosComplement);
+}
+
+dsp::CASPSRUnpacker::~CASPSRUnpacker ()
+{
+}
+
+double dsp::CASPSRUnpacker::get_optimal_variance ()
+{
+  if (!table)
+    throw Error (InvalidState, "dsp::CASPSRUnpacker::get_optimal_variance", 
+		 "BitTable not set");
+
+return table->get_optimal_variance();
+}
+
+void dsp::CASPSRUnpacker::set_table(BitTable* _table)
+{
+  if (verbose)
+    cerr << "dsp::CASPSRUnpacker::set_table" << endl;
+
+  table = _table;
+}
+
+const dsp::BitTable* dsp::CASPSRUnpacker::get_table() const
+{
+  return table;
 }
 
 bool dsp::CASPSRUnpacker::matches (const Observation* observation)
 {
-  return observation->get_machine() == "CASPSR" 
+  //  return observation->get_machine()== "MULTIPLEX"
+  //  && observation->get_nbit() == 8;
+
+  return observation->get_machine()== "CASPSR"
     && observation->get_nbit() == 8;
-    //&& observation->get_state() == Signal::Analytic;
+
+  
 }
 
+void dsp::CASPSRUnpacker::unpack(uint64_t ndat,
+				    const unsigned char* from,
+				    const unsigned nskip,
+				    float* into,
+				    const unsigned fskip,
+				    unsigned long* hist)
+{
+  const float* lookup = table->get_values ();
+  int counter_four = 0;
+
+  if (verbose)
+    cerr << "dsp::CASPSRUnpacker::unpack ndat=" << ndat << endl;
+
+  for (uint64_t idat = 0; idat < ndat; idat++)
+  {
+    hist[ *from ] ++;
+    *into = lookup[ *from ];
+    
+#ifdef _DEBUG
+    cerr << idat << " " << int(*from) << "=" << *into << endl;
+#endif
+    counter_four++;
+    if (counter_four == 4)
+      {
+	counter_four = 0;
+	from += 5; //(nskip+4);
+      }
+    else
+      {
+	from ++; //=nskip;
+      }
+    into += fskip;
+  }
+}
+
+void dsp::CASPSRUnpacker::unpack ()
+{
+  const uint64_t   ndat  = input->get_ndat();
+  
+  const unsigned nchan = input->get_nchan();
+  const unsigned npol  = input->get_npol();
+  const unsigned ndim  = input->get_ndim();
+  
+  const unsigned nskip = npol * nchan * ndim;
+  const unsigned fskip = ndim;
+  
+  unsigned offset = 0;
+  
+  for (unsigned ichan=0; ichan<nchan; ichan++)
+    {
+      for (unsigned ipol=0; ipol<npol; ipol++)
+	{
+	  if (ipol==1)
+	    offset = 4;
+	  for (unsigned idim=0; idim<ndim; idim++)
+	    {
+	      const unsigned char* from = input->get_rawptr() + offset;
+	      float* into = output->get_datptr (ichan, ipol) + idim;
+	      unsigned long* hist = get_histogram (ipol);
+	      
+#ifdef _DEBUG
+	      cerr << "c=" << ichan << " p=" << ipol << " d=" << idim << endl;
+#endif
+	      
+	      unpack (ndat, from, nskip, into, fskip, hist);
+	      offset ++;
+	    }
+	}
+    }
+}
--- Kernel/Formats/caspsr/dsp/CASPSRUnpacker.h
+++ Kernel/Formats/caspsr/dsp/CASPSRUnpacker.h
@@ -1,40 +1,47 @@
-//-*-C++-*-
-/***************************************************************************
- *
- *   Copyright (C) 2009 by Willem van Straten
- *   Licensed under the Academic Free License version 2.1
- *
- ***************************************************************************/
+/*
 
-/* $Source: /cvsroot/dspsr/dspsr/Kernel/Formats/caspsr/dsp/CASPSRUnpacker.h,v $
-   $Revision: 1.1 $
-   $Date: 2009/04/09 02:06:40 $
-   $Author: straten $ */
+ */
 
-#ifndef __CASPSRUnpacker_h
-#define __CASPSRUnpacker_h
+#ifndef __dsp_CASPSRUnpacker_h
+#define __dsp_CASPSRUnpacker_h
 
-#include "dsp/EightBitUnpacker.h"
-
+#include "dsp/HistUnpacker.h"
+//#include "dsp/CASPSRUnpacker.h"
 namespace dsp {
-
-  //! Simple 8-bit to float unpacker for CASPSR files
-  class CASPSRUnpacker : public EightBitUnpacker
+  
+  //
+  class BitTable;
+  
+  class CASPSRUnpacker : public HistUnpacker
   {
-
   public:
-    
-    //! Constructor
-    CASPSRUnpacker (const char* name = "CASPSRUnpacker");
 
-   protected:
+    //! Constructor
+    CASPSRUnpacker (const char* name = "CASPSRUpacker");
+
+    //! Destructor
+    virtual ~CASPSRUnpacker ();
+
+    double get_optimal_variance ();
+
+    void set_table (BitTable* table);
+
+    const BitTable* get_table () const;
+
+
+    void unpack (uint64_t ndat,
+			 const unsigned char* from, const unsigned nskip,
+			 float* into, const unsigned fskip,
+			 unsigned long* hist);
+  protected:
+    
+    Reference::To<BitTable> table;
 
     //! Return true if we can convert the Observation
     virtual bool matches (const Observation* observation);
 
+    virtual void unpack ();
   };
-
 }
 
-#endif // !defined(__CASPSRUnpacker_h)
-
+#endif
