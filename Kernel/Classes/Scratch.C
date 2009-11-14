4389e7486a4844ca3fe2bbbbcdec62fce99ce545
straten <straten>
2009-11-14 12:21:01 +0000

    allocate and free memory using dsp::Memory policy

--- Kernel/Classes/Scratch.C
+++ Kernel/Classes/Scratch.C
@@ -5,19 +5,12 @@
  *
  ***************************************************************************/
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef HAVE_MALLOC_H
-#include <malloc.h>
-#endif
+#include "dsp/Scratch.h"
+#include "dsp/Memory.h"
 
 #include <stdlib.h>
 
 using namespace std;
-
-#include "dsp/Scratch.h"
 
 // default scratch space used by Operations
 static dsp::Scratch* the_default_scratch = 0;
@@ -48,23 +41,17 @@ void* dsp::Scratch::space (size_t nbytes)
        << " current=" << working_size << endl;
 #endif
 
-  if (!nbytes) {
-    if (working_space) free(working_space); working_space = 0;
-    working_size = 0;
+  if (!nbytes || working_size < nbytes)
+  {
+    if (working_space) Memory::free (working_space); working_space = 0;
   }
 
-  if (working_size < nbytes) {
+  if (!nbytes)
+    return 0;
 
-    if (working_space) free(working_space); working_space = 0;
-
-#ifdef HAVE_MALLOC_H
-#ifdef _DEBUG
-    cerr << "dsp::Scratch::space memalign (16," << nbytes << ")" << endl;
-#endif
-    working_space = (char*)memalign(16, nbytes);
-#else
-    working_space = (char*)valloc(nbytes);
-#endif
+  if (working_space == 0)
+  {
+    working_space = (char*) Memory::allocate (nbytes);
 
     if (!working_space)
       throw Error (BadAllocation, "Scratch::space",
