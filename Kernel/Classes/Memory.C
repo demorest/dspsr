2cdfb7279ca30742fdd4efa1315d907215383002
straten <straten>
2009-11-14 12:21:06 +0000

    maintain a default policy

--- Kernel/Classes/Memory.C
+++ Kernel/Classes/Memory.C
@@ -7,15 +7,36 @@
 
 #include "dsp/Memory.h"
 #include "malloc16.h"
+#include <assert.h>
 
-void* dsp::Memory::allocate (unsigned nbytes)
+dsp::Memory* dsp::Memory::manager = new dsp::Memory;
+
+void* dsp::Memory::do_allocate (unsigned nbytes)
 {
   // cerr << "dsp::Memory::allocate malloc16 (" << nbytes << ")" << endl;
   return malloc16 (nbytes);
 }
 
-void dsp::Memory::free (void* ptr)
+void dsp::Memory::do_free (void* ptr)
 {
   // cerr << "dsp::Memory::free free16 (" << ptr << ")" << endl;
   free16 (ptr);
 }
+
+void* dsp::Memory::allocate (unsigned nbytes)
+{
+  return manager->do_allocate (nbytes);
+}
+
+void dsp::Memory::free (void* ptr)
+{
+  manager->do_free (ptr);
+}
+
+void dsp::Memory::set_manager (Memory* new_do)
+{
+  assert (new_do != 0);
+  delete manager;
+  manager = new_do;
+}
+
