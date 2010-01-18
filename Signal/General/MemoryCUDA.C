0c187f8eb8227e9bb1b3179a0995a91bd57cccd4
straten <straten>
2010-01-18 04:00:49 +0000

    differentiate between pinned host memory and device memory

--- Signal/General/MemoryCUDA.C
+++ Signal/General/MemoryCUDA.C
@@ -12,7 +12,7 @@
 #include <iostream>
 using namespace std;
 
-void* CUDA::Memory::do_allocate (unsigned nbytes)
+void* CUDA::PinnedMemory::do_allocate (unsigned nbytes)
 {
   cerr << "CUDA::Memory::allocate cudaMallocHost (" << nbytes << ")" << endl;
   void* ptr = 0;
@@ -20,8 +20,22 @@ void* CUDA::Memory::do_allocate (unsigned nbytes)
   return ptr;
 }
 
-void CUDA::Memory::do_free (void* ptr)
+void CUDA::PinnedMemory::do_free (void* ptr)
 {
   cerr << "CUDA::Memory::free cudaFreeHost (" << ptr << ")" << endl;
   cudaFreeHost (ptr);
+}
+
+void* CUDA::DeviceMemory::do_allocate (unsigned nbytes)
+{
+  cerr << "CUDA::Memory::allocate cudaMalloc (" << nbytes << ")" << endl;
+  void* ptr = 0;
+  cudaMalloc (&ptr, nbytes);
+  return ptr;
+}
+
+void CUDA::DeviceMemory::do_free (void* ptr)
+{
+  cerr << "CUDA::Memory::free cudaFree (" << ptr << ")" << endl;
+  cudaFree (ptr);
 }
