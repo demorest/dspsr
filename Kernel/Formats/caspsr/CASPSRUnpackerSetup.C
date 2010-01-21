1dee91e292d1f0096c71f76cf659cb6008faabc2
tcaotiaafoc <tcaotiaafoc>
2010-01-21 01:14:43 +0000

    Modified Files:  	CASPSRUnpackerSetup.C

--- Kernel/Formats/caspsr/CASPSRUnpackerSetup.C
+++ Kernel/Formats/caspsr/CASPSRUnpackerSetup.C
@@ -28,27 +28,43 @@ void dsp::CASPSRUnpackerSetup::unpack()
     
   // in theory we already know what all of these values are...
   //const uint64_t ndat  = input->get_ndat();
+
+  // staging buffer on the GPU for packed data
+  //unsigned char* stagingBufGPU; -- should not be necessary, cause it'll be
+  //                              -- directly pointed to by get_rawptr();
+
+  //! buffer for unpacked data on the GPU
+  float* unpackBufGPU;
+  
+  //unsigned char* host_mem; -- replaced by "input->get_rawptr()"
+  
   
   const uint64_t ndat = input->get_ndat();
-  uint64_t dataSize = ndat;
+  const unsigned char* stagingBufGPU = input->get_rawptr();
+  float* into_pola = output->getdatptr(0,0);
+  float* into_polb = output->getdatptr(0,1);
+
+
+  //uint64_t dataSize = ndat;
   int dimBlockUnpack(512);
-  int dimGridUnpack(dataSize / dimBlockUnpack*8); // must be less than 33Mpt. Otherwise should be ok. (Max thread per block 512, max grid dim 65535)
+  int dimGridUnpack(dataSize / dimBlockUnpack*8); 
 
   //int packedDim = (nchan * bwd_nfft)/4 ;
-  unsigned packedDim = ndat/4;
-  unsigned halfData = dataSize / 2;
+  //unsigned packedDim = ndat/4;
+  unsigned halfData = ndat / 2;
 
   // buffer on host - should already be allocated
 
   // buffer on gpu for packed data
-  cutilSafeCall(cudaMalloc((void **) &stagingBufGPU,packedDim*sizeof(cufftReal)));
+  //cutilSafeCall(cudaMalloc((void **) &stagingBufGPU,packedDim*sizeof(cufftReal)));
 
-  cutilSafeCall(cudaMemcpy(stagingBufGPU,host_mem,packedDim*sizeof(cufftReal),cudaMemcpyHostToDevice));
+  //cutilSafeCall(cudaMemcpy(stagingBufGPU,from,packedDim*sizeof(cufftReal),cudaMemcpyHostToDevice));
 
   // buffer on gpu for unpacked data
-  cutilSafeCall(cudaMalloc((void **) &unpackBufGPU,packedDim*sizeof(cufftReal)*4));
+  // should be malloc earlier along with the stagin buf? just need the pointers getdatprt? 
+  cutilSafeCall(cudaMalloc((void **) &unpackBufGPU,ndat*sizeof(cufftReal)));
 
   //call function
-  caspsr_unpack(ndat,host_mem,stagingBufGPU,unpackBufGPU,dimBlockUnpack,dimGridUnpack,halfData);
+  caspsr_unpack(ndat,stagingBufGPU,unpackBufGPU,dimBlockUnpack,dimGridUnpack,halfData,into_pola,into_polb);
 }
 
