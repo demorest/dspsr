602b1e9dcdf2f5402f785e9debf3ebdac7729dc5
tcaotiaafoc <tcaotiaafoc>
2010-01-21 02:23:22 +0000

    Modified Files:  	CASPSRUnpackerSetup.C

--- Kernel/Formats/caspsr/CASPSRUnpackerSetup.C
+++ Kernel/Formats/caspsr/CASPSRUnpackerSetup.C
@@ -33,21 +33,21 @@ void dsp::CASPSRUnpackerSetup::unpack()
   //unsigned char* stagingBufGPU; -- should not be necessary, cause it'll be
   //                              -- directly pointed to by get_rawptr();
 
-  //! buffer for unpacked data on the GPU
-  float* unpackBufGPU;
+  // buffer for unpacked data on the GPU
+  //float* unpackBufGPU; -- replaced by "output->get_datptr"
   
   //unsigned char* host_mem; -- replaced by "input->get_rawptr()"
   
   
   const uint64_t ndat = input->get_ndat();
   const unsigned char* stagingBufGPU = input->get_rawptr();
-  float* into_pola = output->getdatptr(0,0);
-  float* into_polb = output->getdatptr(0,1);
+  float* into_pola = output->get_datptr(0,0);
+  float* into_polb = output->get_datptr(0,1);
 
 
   //uint64_t dataSize = ndat;
   int dimBlockUnpack(512);
-  int dimGridUnpack(dataSize / dimBlockUnpack*8); 
+  int dimGridUnpack(ndat / dimBlockUnpack*8); 
 
   //int packedDim = (nchan * bwd_nfft)/4 ;
   //unsigned packedDim = ndat/4;
@@ -62,9 +62,9 @@ void dsp::CASPSRUnpackerSetup::unpack()
 
   // buffer on gpu for unpacked data
   // should be malloc earlier along with the stagin buf? just need the pointers getdatprt? 
-  cutilSafeCall(cudaMalloc((void **) &unpackBufGPU,ndat*sizeof(cufftReal)));
+  //cutilSafeCall(cudaMalloc((void **) &unpackBufGPU,ndat*sizeof(cufftReal)));
 
   //call function
-  caspsr_unpack(ndat,stagingBufGPU,unpackBufGPU,dimBlockUnpack,dimGridUnpack,halfData,into_pola,into_polb);
+  caspsr_unpack(ndat,stagingBufGPU,dimBlockUnpack,dimGridUnpack,halfData,into_pola,into_polb);
 }
 
