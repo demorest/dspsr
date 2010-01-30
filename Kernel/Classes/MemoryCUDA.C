3999adeac404f029820da4104cf8f5d5991643f2
straten <straten>
2010-01-30 09:50:15 +0000

    Memory CUDA moved from Signal/General to Kernel/Classes

---
+++ Kernel/Classes/MemoryCUDA.C
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2009 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+//#define _DEBUG
+
+#include "dsp/MemoryCUDA.h"
+#include "debug.h"
+
+#include <cuda_runtime.h>
+#include <cutil_inline.h>
+
+#include <iostream>
+using namespace std;
+
+/***************************************************************************
+ *
+ *   Pinned memory on host
+ *
+ ***************************************************************************/
+
+void* CUDA::PinnedMemory::do_allocate (unsigned nbytes)
+{
+  DEBUG("CUDA::PinnedMemory::allocate cudaMallocHost (" << nbytes << ")");
+  void* ptr = 0;
+  cudaMallocHost (&ptr, nbytes);
+  return ptr;
+}
+
+void CUDA::PinnedMemory::do_free (void* ptr)
+{
+  DEBUG("CUDA::PinnedMemory::free cudaFreeHost (" << ptr << ")");
+  cudaFreeHost (ptr);
+}
+
+void CUDA::PinnedMemory::do_copy (void* to, const void* from, size_t bytes)
+{
+  DEBUG("CUDA::PinnedMemory::copy (" << to <<","<< from <<","<< bytes << ")");
+  cudaMemcpy (to, from, bytes, cudaMemcpyHostToHost);
+}
+
+/***************************************************************************
+ *
+ *   Memory on device
+ *
+ ***************************************************************************/
+
+void* CUDA::DeviceMemory::do_allocate (unsigned nbytes)
+{
+  DEBUG("CUDA::DeviceMemory::allocate cudaMalloc (" << nbytes << ")");
+  void* ptr = 0;
+  cutilSafeCall( cudaMalloc (&ptr, nbytes) );
+  cudaError err = cudaGetLastError ();
+  if (err != cudaSuccess)
+    throw Error (InvalidState, "CUDA::DeviceMemory::do_allocate",
+                 "cudaMalloc failed: %s", cudaGetErrorString(err));
+
+  DEBUG("CUDA::DeviceMemory::allocate cudaMalloc ptr=" << ptr);
+  return ptr;
+}
+
+void CUDA::DeviceMemory::do_free (void* ptr)
+{
+  DEBUG("CUDA::DeviceMemory::free cudaFree (" << ptr << ")");
+  cudaFree (ptr);
+}
+
+void CUDA::DeviceMemory::do_copy (void* to, const void* from, size_t bytes)
+{
+  DEBUG("CUDA::DeviceMemory::copy (" << to <<","<< from <<","<< bytes << ")");
+  cudaMemcpy (to, from, bytes, cudaMemcpyDeviceToDevice);
+}
+
