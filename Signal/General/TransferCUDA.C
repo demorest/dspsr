81dec14f2c12f44aacb9f58561e6aafd16c00805
straten <straten>
2010-02-02 12:15:33 +0000

    memcpy multiple contiguous blocks in one go

--- Signal/General/TransferCUDA.C
+++ Signal/General/TransferCUDA.C
@@ -27,12 +27,40 @@ void dsp::TransferCUDA::transformation ()
   const uint64_t nbyte = ndat * ndim * sizeof(float);
 
   prepare ();
-  
+
+  unsigned ichan = 0;
+  unsigned ipol = 0;
+
+  const float* from = input->get_datptr( ichan, ipol );
+  float* to = output->get_datptr( ichan, ipol );
+
+  // check for contiguity
+
+  if (nchan > 1)
+    ichan = 1;
+  if (npol > 1)
+    ipol = 1;
+
+  if (ichan || ipol)
+  {
+    // check the pointers of the next blocks
+    const float* from2 = input->get_datptr( ichan, ipol );
+    float* to2 = input->get_datptr( ichan, ipol );
+
+    if ( (from2 - from == ndat * ndim)
+	 && (to2 - to == ndat * ndim) )
+    {
+      cerr << "dsp::TransferCUDA::transformation contiguous blocks" << endl;
+      nbyte *= npol * nchan;
+      cudaMemcpy (to, from, nbyte, kind);
+      return;
+    }
+  }
+
   for (unsigned ipol=0; ipol < npol; ipol++)
     for (unsigned ichan=0; ichan < nchan; ichan++)
     {
       void* to = output->get_datptr( ichan, ipol );
-      const void* from = input->get_datptr( ichan, ipol );
       cudaMemcpy (to, from, nbyte, kind);
     }
 }
