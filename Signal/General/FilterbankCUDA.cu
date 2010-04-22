138c1532bf783b42e61a05e17d4cf37fa571dee4
straten <straten>
2010-04-22 03:17:20 +0000

    use realtr by default

--- Signal/General/FilterbankCUDA.cu
+++ Signal/General/FilterbankCUDA.cu
@@ -32,6 +32,7 @@ void CUDA::Engine::setup (unsigned _nchan, unsigned _bwd_nfft, float* _kernel)
   bwd_nfft = _bwd_nfft;
   nchan = _nchan;
   kernel = _kernel;
+  dual_poln = false;
 }
 
 void CUDA::Engine::init ()
@@ -43,15 +44,50 @@ void CUDA::Engine::init ()
 
   DEBUG("CUDA::Engine::init data_size=" << data_size);
 
-  cufftPlan1d (&plan_fwd, bwd_nfft*nchan*2, CUFFT_C2C, 1);
+  // if using the twofft trick, double the forward/backward fft length
+  unsigned npol = 1;
+  if (dual_poln)
+    npol = 2;
 
-  cufftPlan1d (&plan_bwd, bwd_nfft, CUFFT_C2C, nchan*2);
+  cufftPlan1d (&plan_fwd, bwd_nfft*nchan*npol, CUFFT_C2C, 1);
+
+  cufftPlan1d (&plan_bwd, bwd_nfft, CUFFT_C2C, nchan*npol);
 
   // allocate space for the convolution kernel
   cudaMalloc ((void**)&d_kernel, mem_size);
  
   // copy the kernel accross
   cudaMemcpy (d_kernel, kernel, mem_size, cudaMemcpyHostToDevice);
+
+  if (dual_poln)
+    return;
+
+  unsigned n_half = nchan * bwd_nfft / 2 + 1;
+  unsigned n_half_size = n_half * sizeof(cufftReal);
+
+  // setup realtr coeffients and variables, copy to CUDA
+  double arg = 1.570796327 / (nchan * bwd_nfft); 
+  double CD = 2*sin(arg)*sin(arg);
+  double SD = sin(arg+arg);
+  
+  // used by realtr SN and CN to be copied to kernel
+  std::vector<float> SN (n_half);
+  std::vector<float> CN (n_half);
+
+  SN[0]=0.0;
+  CN[0]=1.0;
+
+  for (int j=1; j<n_half; j++)
+  {
+    CN[j]=CN[j-1]-(CD*CN[j-1]+SD*SN[j-1]);
+    SN[j]=(SD*CN[j-1]-CD*SN[j-1])+SN[j-1];
+  }
+
+  cudaMalloc((void**)&d_CN, n_half_size);
+  cudaMalloc((void**)&d_SN, n_half_size);
+
+  cudaMemcpy(d_CN,&(CN[0]),n_half_size,cudaMemcpyHostToDevice);
+  cudaMemcpy(d_SN,&(SN[0]),n_half_size,cudaMemcpyHostToDevice);
 }
 
 void check_error (const char* method)
@@ -74,21 +110,26 @@ void CUDA::Engine::finish ()
   check_error ("CUDA::Engine::finish");
 }
 
-/*
-  Where:
-    Z = X + i Y
-    X, Y, and Z are complex
-    X(-w) = X*(w)
-    Y(-w) = X*(w)
-    Z^*(-w) = X(w) - i Y(w)
-*/
+/* *************************************************************************
+ *
+ *
+ * The twofft trick
+ *
+ * Where:
+ *   Z = X + i Y
+ *   X, Y, and Z are complex
+ *   X(-w) = X*(w)
+ *   Y(-w) = X*(w)
+ *   Z^*(-w) = X(w) - i Y(w)
+ *
+ *
+ ************************************************************************* */
 
 // compute 2X(w) = Z(w) + Z^*(-w) 
 #define sep_X(X,z,zh) X.x = 0.5*(z.x + zh.x); X.y = 0.5*(z.y - zh.y);
 
 // compute 2Y(w) = iZ^*(-w) - iZ(w)
 #define sep_Y(Y,z,zh) Y.x = 0.5*(zh.y + z.y); Y.y = 0.5*(zh.x - z.x);
-
 
 __global__ void separate (float2* d_fft, int nfft)
 {
@@ -110,6 +151,52 @@ __global__ void separate (float2* d_fft, int nfft)
   sep_Y( p1[i], p0i, p1k );
   sep_Y( p1[k], p0k, p1i );
 }
+
+/* *************************************************************************
+ *
+ *
+ * The realtr trick
+ *
+ *
+ ************************************************************************* */
+
+__global__ void performRealtr (float2* d_fft, unsigned bwd_nfft,
+			       float* k_SN, float* k_CN)
+{
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  int k = bwd_nfft - i;
+ 
+  float real_aa, real_ab, imag_ba, imag_bb, temp_real, temp_imag;
+
+  // eg Final result = 16 elements. N = 8, NK = 8, NH = 4;
+
+  //fprintf(stderr,"i : %d\n", i);
+  //fprintf(stderr,"k : %d\n", k);
+ 
+  
+  real_aa=d_fft[i].x+d_fft[k].x;
+  real_ab=d_fft[k].x-d_fft[i].x;
+  
+  imag_ba=d_fft[i].y+d_fft[k].y;
+  imag_bb=d_fft[k].y-d_fft[i].y;
+
+  temp_real=k_CN[i]*imag_ba+k_SN[i]*real_ab;
+  temp_imag=k_SN[i]*imag_ba-k_CN[i]*real_ab;
+
+  d_fft[k].y = -0.5*(temp_imag-imag_bb);
+  d_fft[i].y = -0.5*(temp_imag+imag_bb);
+
+  d_fft[k].x = 0.5*(real_aa-temp_real);
+  d_fft[i].x = 0.5*(real_aa+temp_real);
+}
+
+/* *************************************************************************
+ *
+ *
+ * end of tricks
+ *
+ *
+ ************************************************************************* */
 
 __global__ void multiply (float2* d_fft, float2* kernel)
 {
@@ -152,7 +239,10 @@ void CUDA::Engine::perform (const float* in)
   // note that each thread will set two complex numbers in each poln
   int blocks = data_size / (threads*2);
 
-  separate<<<blocks,threads>>> (cscratch, data_size);
+  if (dual_poln)
+    separate<<<blocks,threads>>> (cscratch, data_size);
+  else
+    performRealtr<<<blocks,threads>>> (cscratch,data_size,d_SN,d_CN);
  
   CHECK_ERROR ("CUDA::Engine::perform separate");
 
