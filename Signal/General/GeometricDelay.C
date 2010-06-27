523bc13997e21d59fac1abcab38f0484ee4ff76b
straten <straten>
2010-06-27 10:56:21 +0000

    add a response function to modify polarized bandpasses

--- Signal/General/GeometricDelay.C
+++ Signal/General/GeometricDelay.C
@@ -8,6 +8,8 @@
 #include "dsp/GeometricDelay.h"
 #include "dsp/Observation.h"
 
+#include <complex>
+
 using namespace std;
 
 dsp::GeometricDelay::GeometricDelay ()
@@ -25,12 +27,46 @@ bool dsp::GeometricDelay::match (const Observation* obs)
 
     The delays should be stored for later use by get_delay
   */  
+
+  delay[0] = 0;
+  delay[1] = 4;
+
+  unsigned npol  = 2;    // representing two telescope
+  unsigned nchan = 1;    // a single, undivide band of baseband data
+  unsigned ndim  = 2;    // complex-valued response function
+  uint64_t ndat  = 1024; // HERE: required frequency resolution
+
+  response.resize (npol, nchan, ndat, ndim);
+
+  // constant index
+  const unsigned ichan = 0;
+
+  // ///////////////////////////////////////////////////////////////////////
+  //
+  // setup for telescope 0
+  //
+  float* datptr = response.get_datptr (ichan, 0 /*telecope*/);
+
+  complex<float>* phasors = (complex<float>*) datptr;
+  for (unsigned idat=0; idat<ndat; idat++)
+    phasors[idat] = polar (double(1.0), -1.0*idat*delay[0]*M_PI/ndat /* HERE */);
+
+  // ///////////////////////////////////////////////////////////////////////
+  //
+  // setup for telescope 1
+  //
+  datptr = response.get_datptr (ichan, 1 /*telecope*/);
+
+  phasors = (complex<float>*) datptr;
+  for (unsigned idat=0; idat<ndat; idat++)
+    phasors[idat] = polar (double(1.0), -1.0*idat*delay[1]*M_PI/ndat /* HERE */);
+
   return true;
 }
 
 //! Return the geometric delay for the given polarization
 int64_t dsp::GeometricDelay::get_delay (unsigned ichan, unsigned ipol)
 {
-  return 0; // delays[ipol];
+  return delay[ipol];
 }
 
