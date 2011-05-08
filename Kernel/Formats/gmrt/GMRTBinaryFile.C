0c1d96d59a8ad56c3a01562845000dcbf9865039
straten <straten>
2011-05-08 07:02:00 +0000

    binary file with separate ascii header

---
+++ Kernel/Formats/gmrt/GMRTBinaryFile.C
@@ -0,0 +1,90 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2011 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "dsp/GMRTBinaryFile.h"
+#include "dsp/ASCIIObservation.h"
+#include "Error.h"
+
+#include <fstream>
+#include <fcntl.h>
+
+using namespace std;
+
+dsp::GMRTBinaryFile::GMRTBinaryFile (const char* filename) : File ("GMRT")
+{
+  if (filename) 
+    open (filename);
+}
+
+
+string dsp::GMRTBinaryFile::get_header (const char* filename)
+{
+  string hdr_filename = filename + string(".hdr");
+
+  ifstream input ( hdr_filename.c_str() );
+
+  string line;
+
+  if (!input)
+  {
+    if (verbose)
+      std::cerr << "dsp::GMRTBinaryFile::get_header"
+	" could not open '" << hdr_filename << "'" << endl;
+    return line;
+  }
+
+  getline (input, line, '\0');
+
+  return line;
+}
+
+bool dsp::GMRTBinaryFile::is_valid (const char* filename) const
+{
+  string header = get_header (filename);
+
+  if (header.empty())
+  {
+    if (verbose)
+      cerr << "dsp::GMRTBinaryFile::is_valid empty header" << endl;
+    return false;
+  }
+
+  try
+  {
+    ASCIIObservation test;
+    test.load( header.c_str() );
+    return true;
+  }
+  catch (Error& error)
+    {
+      if (verbose)
+	cerr << "dsp::GMRTBinaryFile::is_valid "
+	     << error.get_message() << endl;
+    }
+
+  return false;
+}
+
+void dsp::GMRTBinaryFile::open_file (const char* filename)
+{  
+  string header = get_header (filename);
+
+  if (header.empty())
+    throw Error (FailedCall, "dsp::GMRTBinaryFile::open_file",
+		 "get_header(%s) failed", filename);
+  
+  ASCIIObservation data (header.c_str());
+  info = data;
+
+  header_bytes = 0;
+
+  resolution = info.get_nsamples (1);
+  if (resolution == 0)
+    resolution = 1;
+
+  open_fd (filename);
+}
---
+++ Kernel/Formats/gmrt/dsp/GMRTBinaryFile.h
@@ -0,0 +1,47 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2011 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+/* $Source: /cvsroot/dspsr/dspsr/Kernel/Formats/gmrt/dsp/GMRTBinaryFile.h,v $
+   $Revision: 1.1 $
+   $Date: 2011/05/08 07:02:00 $
+   $Author: straten $ */
+
+
+#ifndef __GMRTBinaryFile_h
+#define __GMRTBinaryFile_h
+
+#include "dsp/File.h"
+
+namespace dsp {
+
+  //! Loads BitSeries data from a GMRTBinary data file
+  class GMRTBinaryFile : public File 
+  {
+
+  public:
+   
+    //! Construct and open file
+    GMRTBinaryFile (const char* filename=0);
+
+    //! Returns true if filename appears to name a valid GMRTBinary file
+    bool is_valid (const char* filename) const;
+
+  protected:
+
+    //! Open the file
+    virtual void open_file (const char* filename);
+
+    //! Read the ascii header from filename.hdr
+    static std::string get_header (const char* filename);
+
+  };
+
+}
+
+#endif // !defined(__GMRTBinaryFile_h)
+  
