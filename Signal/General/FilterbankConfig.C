63cfae891aad007b3d8c7426e4f4647cb9b919e6
straten <straten>
2011-07-15 04:18:11 +0000

    3367609: encapsulates the dspsr -F command line option for export to digifil

--- Signal/General/FilterbankConfig.C
+++ Signal/General/FilterbankConfig.C
@@ -12,42 +12,55 @@ using dsp::Filterbank;
 Filterbank::Config::Config ()
 {
   nchan = 1;
+  freq_res = 0;  // unspecified
   when = After;  // not good, but the original default
 }
 
-std::ostream& operator << (std::ostream& os, const Filterbank::Config& config)
+std::ostream& dsp::operator << (std::ostream& os,
+				const Filterbank::Config& config)
 {
   os << config.get_nchan();
-  if (config.get_dedisperse_when() == Filterbank::Config::Before)
+  if (config.get_convolve_when() == Filterbank::Config::Before)
     os << ":B";
-  else if (config.get_dedisperse_when() == Filterbank::Config::During)
+  else if (config.get_convolve_when() == Filterbank::Config::During)
     os << ":D";
+  else if (config.get_freq_res() != 1)
+    os << ":" << config.get_freq_res();
+
   return os;
 }
 
 //! Extraction operator
-std::istream& operator >> (std::istream& is, Filterbank::Config& config)
+std::istream& dsp::operator >> (std::istream& is, Filterbank::Config& config)
 {
   unsigned value;
   is >> value;
 
   config.set_nchan (value);
-  config.set_dedisperse_when (Filterbank::Config::After);
+  config.set_convolve_when (Filterbank::Config::After);
 
-  if (!is)
+  if (!is || is.peek() != ':')
     return is;
 
-  std::streampos pos = is.tellg();
+  // throw away the colon
+  is.get();
 
-  std::string when;
-  is >> when;
-
-  if (when == ":D" || when == ":d")
-    config.set_dedisperse_when (Filterbank::Config::During);
-  else if (when == ":B" || when == ":b")
-    config.set_dedisperse_when (Filterbank::Config::Before);
-  else if (when.length())
-    is.seekg (pos);
+  if (is.peek() == 'D' || is.peek() == 'd')
+  {
+    is.get();  // throw away the D
+    config.set_convolve_when (Filterbank::Config::During);
+  }
+  else if (is.peek() == 'B' || is.peek() == 'b')
+  {
+    is.get();  // throw away the B
+    config.set_convolve_when (Filterbank::Config::Before);
+  }
+  else
+  {
+    unsigned nfft;
+    is >> nfft;
+    config.set_freq_res (nfft);
+  }
 
   return is;
 }
--- Signal/General/dsp/FilterbankConfig.h
+++ Signal/General/dsp/FilterbankConfig.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/dspsr/dspsr/Signal/General/dsp/FilterbankConfig.h,v $
-   $Revision: 1.1 $
-   $Date: 2011/07/15 02:56:38 $
+   $Revision: 1.2 $
+   $Date: 2011/07/15 04:18:11 $
    $Author: straten $ */
 
 #ifndef __FilterbankConfig_h
@@ -32,15 +32,19 @@ namespace dsp
 
     Config ();
 
-    void set_nchan (unsigned);
+    void set_nchan (unsigned n) { nchan = n; }
     unsigned get_nchan () const { return nchan; }
 
-    void set_dedisperse_when (When);
-    When get_dedisperse_when () const { return when; }
+    void set_freq_res (unsigned n) { freq_res = n; }
+    unsigned get_freq_res () const { return freq_res; }
+
+    void set_convolve_when (When w) { when = w; }
+    When get_convolve_when () const { return when; }
 
   protected:
 
     unsigned nchan;
+    unsigned freq_res;
     When when;
 
   };
