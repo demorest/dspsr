a1788a79813e99b91d78917460dcf68f6a713a9f
straten <straten>
2011-08-04 21:06:56 +0000

    sk-ajameson merge: TransferPhaseSeriesCUDA retrieves hits array from GPU

---
+++ Signal/Pulsar/dsp/TransferPhaseSeriesCUDA.h
@@ -0,0 +1,44 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2010 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#ifndef __TransferPhaseSeriesCUDA_h
+#define __TransferPhaseSeriesCUDA_h
+
+#include "dsp/Transformation.h"
+#include "dsp/PhaseSeries.h"
+
+#include <cuda_runtime.h>
+
+namespace dsp {
+
+  class TransferPhaseSeriesCUDA : public Transformation<PhaseSeries,PhaseSeries>
+  {
+  public:
+
+    //! Default constructor - always out of place
+    TransferPhaseSeriesCUDA();
+
+    void set_kind (cudaMemcpyKind k) { kind = k; }
+    void prepare ();
+    void set_transfer_hits (bool transfer) { transfer_hits = transfer; }
+
+    Operation::Function get_function () const { return Operation::Structural; }
+
+  protected:
+
+    //! Do stuff
+    void transformation();
+
+    cudaMemcpyKind kind;
+
+    bool transfer_hits;
+  };
+
+}
+
+#endif
