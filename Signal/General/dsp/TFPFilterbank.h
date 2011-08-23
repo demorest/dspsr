d921d38a93516754efb41b100f8ce8708e8ffe23
straten <straten>
2011-08-23 21:00:38 +0000

    no Engine required

--- Signal/General/dsp/TFPFilterbank.h
+++ Signal/General/dsp/TFPFilterbank.h
@@ -7,8 +7,8 @@
  ***************************************************************************/
 
 /* $Source: /cvsroot/dspsr/dspsr/Signal/General/dsp/TFPFilterbank.h,v $
-   $Revision: 1.2 $
-   $Date: 2011/08/04 21:06:12 $
+   $Revision: 1.3 $
+   $Date: 2011/08/23 21:00:38 $
    $Author: straten $ */
 
 #ifndef __TFPFilterbank_h
@@ -28,36 +28,18 @@ namespace dsp {
     //! Null constructor
     TFPFilterbank ();
 
-    //! Engine used to perform discrete convolution step
-    class Engine;
-    void set_engine (Engine*);
-
   protected:
 
     //! Perform the filterbank step 
     virtual void filterbank ();
     virtual void custom_prepare ();
 
-    //! Interface to alternate processing engine (e.g. GPU)
-    Reference::To<Engine> engine;
-
   private:
-
-    //! number of FFTs to scrunch in time
-    unsigned tscrunch;
 
     //! pscrunch flag
     unsigned pscrunch;
-
-    unsigned debugd;
-
   };
- 
-  class TFPFilterbank::Engine : public Reference::Able
-  {
-  public:
-      Engine () {}
-  }; 
+
 }
 
 #endif
