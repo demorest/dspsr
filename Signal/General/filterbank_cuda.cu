1b9f227cdcb9fb7a60927f92ab3501646b941711
gitj <glenn.caltech@gmail.com>
2012-09-28 17:19:32 -0400

    added comments used nchan_subband instead of nchan to be more clear

--- Signal/General/filterbank_cuda.cu
+++ Signal/General/filterbank_cuda.cu
@@ -23,6 +23,8 @@ void check_error (const char*);
 #define CHECK_ERROR(x)
 #endif
 
+// Multiply data in d_fft by kernel with result in d_fft
+// total number of elements multiplied is blockDim.x * gridDim.x
 __global__ void multiply (float2* d_fft, float2* kernel)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
@@ -32,6 +34,9 @@ __global__ void multiply (float2* d_fft, float2* kernel)
   d_fft[i].x = x;
 }
 
+// Copy gridDim.y blocks from input to output. Each copy is blockDim.x * gridDim.x but only values with index < to_copy are actually copied
+// the input blocks are offset by input_stride and the output blocks are offset by output_stride
+// In practice, blockIdx.y is nchan_subband
 __global__ void ncopy (float2* output_data, unsigned output_stride, 
 		       const float2* input_data, unsigned input_stride,
 		       unsigned to_copy)
@@ -52,7 +57,7 @@ void filterbank_cuda_perform (filterbank_engine* engine,
 {
   float2* cscratch = (float2*) engine->scratch;
 
-  unsigned data_size = engine->nchan * engine->freq_res; //cuda->bwd_nfft;
+  unsigned data_size = engine->nchan_subband * engine->freq_res; //cuda->bwd_nfft;
   int threads_per_block = max_threads_per_block / 2;
 
   // note that each thread will set two complex numbers in each poln
@@ -97,8 +102,10 @@ void filterbank_cuda_perform (filterbank_engine* engine,
   }
 
   const float2* input = cscratch + engine->nfilt_pos;
-  unsigned input_stride = engine->freq_res;
+  unsigned input_stride = engine->freq_res; // each output from the reverse FFT is freq_res long
   //unsigned input_stride = cuda->bwd_nfft;
+  //engine->nkeep is set in Filterbank::resize_output()
+  // nkeep = freq_res - nfilt_tot   Thus num points kept from each reverse FFT.
   unsigned to_copy = engine->nkeep;
 
   {
@@ -110,15 +117,19 @@ void filterbank_cuda_perform (filterbank_engine* engine,
     if (engine->nkeep % threads.x)
       blocks.x ++;
 
-    blocks.y = engine->nchan; // this will be 1 for input chan == output chan
+    blocks.y = engine->nchan_subband; // this will be 1 for input chan == output chan
     
     // divide by two for complex data
+    // output_span is number of floats between two consecutive channels in output array
+    // so we divide by 2 because ncopy copies two floats at a time (one complex value represented as a float2)
     float2* output_base = (float2*) engine->output;
     unsigned output_stride = engine->output_span / 2;
     if (cuda->verbose) {
     	cerr << "copy: blocks.x=" << blocks.x << " blocks.y=" << blocks.y;
     	cerr << " output_base=" << output_base << " output stride=" << output_stride << " input=" << input << " input stride=" << input_stride << " tocopy=" << to_copy << endl;
     }
+
+	// Hence ncopy will go through each subchannel in the subband and copy nkeep data points to the output in the corresponding location for the given subchannel
     ncopy<<<blocks,threads,0,cuda->stream>>> (output_base, output_stride,
 					      input, input_stride, to_copy);
   }
