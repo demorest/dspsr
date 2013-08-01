80598a661dcc5337954a44bb5dc679c886133bbd
James M Anderson <anderson271828@gmail.com>
2013-08-01 06:57:47 +0200

    Make sigproc global variable declarations in filterbank.h and header.h "extern", and define the variables locally within filterbank_header.c.  This is necessary to prevent gcc from giving the variables different memory alignments between the C and C++ compiler versions (and possibly different optimization levels, and so on), and then later the linker giving error messages about the different alignments.

--- Kernel/Formats/sigproc/filterbank_header.c
+++ Kernel/Formats/sigproc/filterbank_header.c
@@ -1,5 +1,44 @@
 #include "filterbank.h"
 
+
+/* define the varaiables declared extern in filterbank.h and header.h here */
+/* input and output files and logfile (filterbank.monitor) */
+FILE *input, *output, *logfile;
+char  inpfile[80], outfile[80];
+
+/* global variables describing the data */
+double time_offset;
+
+/* global variables describing the operating mode */
+float start_time, final_time, clip_threshold;
+
+int obits, sumifs, headerless, headerfile, swapout, invert_band;
+int compute_spectra, do_vanvleck, hanning, hamming, zerolagdump;
+int headeronly;
+char ifstream[8];
+
+/* global variables describing the data */
+char rawdatafile[80], source_name[80];
+int machine_id, telescope_id, data_type, nchans, nbits, nifs, scan_number,
+  barycentric,pulsarcentric; /* these two added Aug 20, 2004 DRL */
+double tstart,mjdobs,tsamp,fch1,foff,refdm,az_start,za_start,src_raj,src_dej;
+double gal_l,gal_b,header_tobs,raw_fch1,raw_foff;
+int nbeams, ibeam;
+char isign;
+/* added 20 December 2000    JMC */
+double srcl,srcb;
+double ast0, lst0;
+long wapp_scan_number;
+char project[8];
+char culprits[24];
+double analog_power[2];
+
+/* added frequency table for use with non-contiguous data */
+double frequency_table[4096]; /* note limited number of channels */
+long int npuls; /* added for binary pulse profile format */
+
+
+
 void filterbank_header(FILE *outptr) /* includefile */
 {
   if (sigproc_verbose)
