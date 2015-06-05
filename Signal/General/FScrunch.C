92f5981efc2b3be793a4c5a6d35ff9a5a75e848a
Kerr, Matthew (CASS, Marsfield) <matthew.kerr@gmail.com>
2015-06-05 11:57:09 +1000

    Add checks for empty input.

--- Signal/General/FScrunch.C
+++ Signal/General/FScrunch.C
@@ -80,6 +80,9 @@ void dsp::FScrunch::transformation ()
     return;
   }
 
+  if (input->get_ndat() == 0)
+    return;
+
   if( !input->get_detected() )
     throw Error(InvalidState,"dsp::FScrunch::transformation()",
 		"invalid input state: " + tostring(input->get_state()));
