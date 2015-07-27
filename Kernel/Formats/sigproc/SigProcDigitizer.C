a6d81e4e83610baa441774262d653752a7f5594b
Vlad Kondratiev <vlad.kondratiev@gmail.com>
2015-07-27 11:26:31 +0200

    changed order of checking for flip_band. Otherwise it's resulted in the wrond frequency order

--- Kernel/Formats/sigproc/SigProcDigitizer.C
+++ Kernel/Formats/sigproc/SigProcDigitizer.C
@@ -59,7 +59,7 @@ public:
       in_chan = input->get_unswapped_ichan(out_chan);
     else if (swap_band)
       in_chan = (in_chan+half_chan)%nchan;
-    // moved from the start of the block
+    // moved from the start of the block 
     if (flip_band)
       in_chan = (nchan-in_chan-1);
     return in_chan;
