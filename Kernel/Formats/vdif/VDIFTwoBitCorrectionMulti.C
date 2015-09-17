9687cea76939669dcef02ad26940bb69f5b63b03
Stephen Ord <steve.ord@gmail.com>
2015-09-17 17:15:08 +0800

    Additions to for 2 bit complex sampled VDIF. Needed for MWA

--- Kernel/Formats/vdif/VDIFTwoBitCorrectionMulti.C
+++ Kernel/Formats/vdif/VDIFTwoBitCorrectionMulti.C
@@ -19,18 +19,45 @@ dsp::VDIFTwoBitCorrectionMulti::VDIFTwoBitCorrectionMulti ()
   : SubByteTwoBitCorrection ("VDIFTwoBitCorrectionMulti")
 {
   table = new TwoBitTable (TwoBitTable::OffsetBinary);
-  set_ndig(2);
+  set_ndig(4);
 }
-
-#if 0 
-unsigned
-dsp::VDIFTwoBitCorrectionMulti::get_shift (unsigned idig, unsigned isamp) const
+/*! Each 2-bit sample from each digitizer is packed into one byte */
+unsigned dsp::VDIFTwoBitCorrectionMulti::get_ndig_per_byte () const
 {
-  unsigned shift[4] = { 4, 6, 0, 2 };
-
-  assert (isamp < 2);
-  assert (idig < 2);
-
-  return shift[idig];
+  if (input->get_state() == Signal::Analytic)
+  	return 4;
+  else 
+	return 2;
 }
-#endif
+unsigned dsp::VDIFTwoBitCorrectionMulti::get_output_offset (unsigned idig) const
+{
+  return idig % 2;
+}
+
+/*! The in-phase and quadrature components must be interleaved */
+unsigned dsp::VDIFTwoBitCorrectionMulti::get_output_incr () const
+{
+  return 2;
+}
+/*! The first two digitizer channels are poln0, the last two are poln1 */
+unsigned dsp::VDIFTwoBitCorrectionMulti::get_output_ipol (unsigned idig) const
+{
+  return idig / 2;
+}
+
+unsigned dsp::VDIFTwoBitCorrectionMulti::get_shift (unsigned idig, unsigned isamp) const
+{
+  
+    
+  if (input->get_state() == Signal::Analytic) {
+    unsigned shift[4] = {4,6,0,2};
+
+    return shift[idig] ;
+  }
+  else {
+   // this was the original default shift
+    return (idig + isamp * 2) * 2; 
+  }
+  
+}
+
