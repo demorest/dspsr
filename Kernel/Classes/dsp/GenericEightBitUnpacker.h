fe28dced6a217e52c0f2af55869ef8fff9cb43a6
Willem van Straten <willem@swin.edu.au>
2015-10-14 23:47:17 +1100

    unpack generic 8-bit data on GPU if possible

--- Kernel/Classes/dsp/GenericEightBitUnpacker.h
+++ Kernel/Classes/dsp/GenericEightBitUnpacker.h
@@ -27,11 +27,23 @@ namespace dsp {
     //! Constructor
     GenericEightBitUnpacker ();
 
+    //! Return true if the unpacker can operate on the specified device
+    bool get_device_supported (Memory*) const;
+
+    //! Set the device on which the unpacker will operate
+    void set_device (Memory*);
+
    protected:
 
     //! Return true if this unpacker can convert the Observation
     virtual bool matches (const Observation* observation);
 
+    //! Override BitUnpacker::unpack
+    virtual void unpack ();
+
+    BitSeries staging;
+    void* gpu_stream;
+    void unpack_on_gpu ();
   };
 
 }
