af1186ecc250e5d50e3b71d046beebad7f0bb22a
Andrew Jameson <andrew.jameson@gmail.com>
2016-07-21 17:59:30 +1000

    Removed load_bytes_device, dsp::File implements this

--- Kernel/Classes/dsp/BlockFile.h
+++ Kernel/Classes/dsp/BlockFile.h
@@ -60,10 +60,6 @@ namespace dsp {
       additional information should be filtered out. */
     virtual int64_t load_bytes (unsigned char* buffer, uint64_t nbytes);
 
-#if HAVE_CUDA
-     virtual int64_t load_bytes_device (unsigned char* buffer, uint64_t bytes, void * device_handle);
-#endif
-
     //! Set the file pointer to the absolute number of sampled data bytes
     /*! If the header_bytes attribute is set, this number of bytes
       will be subtracted by File::seek_bytes before seeking.  If the
