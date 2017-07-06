a0792b7f17718cd3a913c0594095e2126c609863
Willem van Straten <vanstraten.willem@gmail.com>
2017-07-06 15:20:37 +1200

    register the Mark5b file loader, which uses Walter Brisken's mark5access library

--- Kernel/Formats/File_registry.C
+++ Kernel/Formats/File_registry.C
@@ -125,6 +125,11 @@ static dsp::File::Register::Enter<dsp::Mark4File> register_mark4;
 static dsp::File::Register::Enter<dsp::Mark5File> register_mark5;
 #endif
 
+#if HAVE_mark5b
+#include "dsp/Mark5bFile.h"
+static dsp::File::Register::Enter<dsp::Mark5bFile> register_mark5b;
+#endif
+
 #if HAVE_maxim
 #include "dsp/MaximFile.h"
 static dsp::File::Register::Enter<dsp::MaximFile> register_maxim;
